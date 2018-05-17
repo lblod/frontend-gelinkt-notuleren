be3026e543ea7fbb4930a2c6b1db42b99203c41e
Miet Claes <miet@miet.be>
2018-05-17 18:40:20 +0200

    Change pill classes to updated css

--- app/components/editor-status-pill.js
+++ app/components/editor-status-pill.js
@@ -8,17 +8,17 @@ export default Component.extend({
     switch(id) {
     case "c272d47d756d4aeaa0be72081f1389c6":
       // ondertekende notulen gepubliceerd
-      return "pill--goedgekeurd";
+      return "pill--gray-dark";
     case "cfd751588a6c453296de9f9c0dff2af4":
     case "5A8304E8C093B00009000010":
       // concept
-      return "pill--concept";
+      return "pill--gray-light";
     case "627aec5d144c422bbd1077022c9b45d1":
       // agenda publiek
-      return "pill--agenda";
+      return "pill--primary-yellow";
     case "b763390a63d548bb977fb4804293084a":
       // notulen publiek
-      return "pill--goedkeuren";
+      return "pill--gray-dark";
     }
     return null;
   })
