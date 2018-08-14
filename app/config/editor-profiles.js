14f413bde53fcaa024105d930a822e75a8f43380
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-08-14 16:13:17 +0200

    installed generic-model-plugin

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -1,11 +1,13 @@
 export default {
   default: [
+    "rdfa-editor-generic-model-plugin",
     "rdfa-editor-standard-template-plugin",
     "mandataris-plugin",
     "rdfa-editor-date-plugin",
     "rdfa-editor-citaten-plugin"
   ],
   all: [
+    "rdfa-editor-generic-model-plugin",
     "rdfa-editor-console-logger-plugin",
     "rdfa-editor-standard-template-plugin",
     "mandataris-plugin",
