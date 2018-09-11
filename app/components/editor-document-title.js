ad20d3dd68423cec9edd6cb16934a7a6c4420884
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-09-11 16:35:06 +0200

    added the automated extraction of the title
     - If no automatic RDFA title is provided, you can provide
       a name yourself
     - Once a rdfa title has been extracted, you cannot edit the title
       in the editable input field -> only through the document
     - If you remove what seems to be title specific rdfa, you'll be able
       to provide title again.
    
    Caveats:
      - currently you have to modify the title in the editor document to
      trigger the change.
      This because the responsible plugin is not aware when a template is inserted (which could contain a title)
      - This issue also raises also some 'weird/unserfriendly' behaviour
        where, once a document is loaded with an extractable RDFA title,
        you're still able to edit the title trough the input field.
    
      These caveats should not be adressed by the consuming app. But on a
      deeper level.

--- app/components/editor-document-title.js
+++ app/components/editor-document-title.js
@@ -1,9 +1,54 @@
 import { schedule } from '@ember/runloop';
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import  { computed } from '@ember/object';
+import EmberObject, {  observer } from '@ember/object';
+
 
 export default Component.extend({
   active: false,
   editorDocument: null,
+  titlePlugin: service('rdfa-editor-document-title-plugin'),
+  overruledTitle: '',
+  generatedTitle: '',
+
+  allowManualOverrule: computed('generatedTitle', function(){
+    if(this.generatedTitle.length > 0){
+      return false;
+    }
+    return true;
+  }),
+
+  title: computed('overruledTitle', 'generatedTitle', function(){
+    if(this.generatedTitle.length > 0){
+      this.set('overruledTitle', '');
+      this.set('editorDocument.title', this.generatedTitle);
+    }
+
+    if(this.overruledTitle.length > 0){
+      this.set('editorDocument.title', this.overruledTitle);
+    }
+
+    if(this.overruledTitle.length == 0 && this.generatedTitle.length == 0){
+      this.set('editorDocument.title', 'Naamloos document');
+    }
+
+    return this.editorDocument.title;
+
+  }),
+
+  titleObserver(){
+    this.set('generatedTitle', this.titlePlugin.title);
+  },
+
+  didReceiveAttrs(){
+    this._super(...arguments);
+    this.set('overruledTitle', this.editorDocument.title || '');
+
+    if(this.titlePlugin)
+      this.titlePlugin.addObserver('title', this.titleObserver.bind(this));
+  },
+
   actions:{
     toggleActive(){
       this.set('active', !this.active);
