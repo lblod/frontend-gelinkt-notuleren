1a9dd7c993b9d3445520a8a434018e6e72282f96
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-05 00:18:13 +0200

    added custom serializer to make mu-cl-resources work with uri

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -2,5 +2,8 @@ import DS from 'ember-data';
 import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 
 export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
-
+    serializeAttribute(snapshot, json, key, attributes) {
+    if (key !== 'uri')
+      this._super(snapshot, json, key, attributes);
+    }
 });
