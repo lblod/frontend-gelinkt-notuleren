80031b4989f9ccdc9126a776a42d91f7e324c46c
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-12 14:22:26 +0200

    linting

--- app/controllers/editor-documents/new.js
+++ app/controllers/editor-documents/new.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import EditorDocumentBaseController from '../../mixins/editor-document-base-controller';
-import { empty } from '@ember/object/computed';
 
 export default Controller.extend(EditorDocumentBaseController, {
   actions: {
