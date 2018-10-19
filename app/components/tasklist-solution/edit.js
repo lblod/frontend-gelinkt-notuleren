6c5dcf63ebd4c8e9d9b91c607f600e5fcdeaf38c
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-19 13:04:17 +0200

    small fixes

--- app/components/tasklist-solution/edit.js
+++ app/components/tasklist-solution/edit.js
@@ -1,4 +1,3 @@
-import Component from '@ember/component';
 import EditTasklistSolution from '@lblod/ember-vo-tasklist/components/tasklist-solution/edit';
 export default EditTasklistSolution.extend({
   classNameBindings: ['open:js-accordion--open'],
