93d222900b84dfe3f62e70711fcbd8de9e3b3370
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-22 18:39:54 +0200

    make sure there is an index

--- app/models/tasklist-solution.js
+++ app/models/tasklist-solution.js
@@ -1,5 +1,6 @@
 import TasklistSolution from '@lblod/ember-vo-tasklist/models/tasklist-solution' ;
 import attr from 'ember-data/attr';
 export default TasklistSolution.extend({
-  uri: attr()
+  uri: attr(),
+  index: attr('number')
 });
