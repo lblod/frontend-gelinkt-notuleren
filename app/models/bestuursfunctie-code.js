0d74b252b17747d979979e08b24dfeb9f9389e53
Niels Vandekeybus <progster@gmail.com>
2018-10-23 13:45:20 +0200

    support for editing installatie table

--- app/models/bestuursfunctie-code.js
+++ app/models/bestuursfunctie-code.js
@@ -1,1 +1,11 @@
-export { default } from '@lblod/ember-rdfa-editor-mandataris-plugin/models/bestuursfunctie-code' ;
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import CodeMixin from '@lblod/ember-rdfa-editor-mandataris-plugin/mixins/bestuursfunctie-code';
+
+export default Model.extend(CodeMixin, {
+  uri: attr(),
+  rdfaBindings: { // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
+    class: "http://www.w3.org/2004/02/skos/core#Concept",
+    label: "http://www.w3.org/2004/02/skos/core#prefLabel"
+   }
+});
--- app/models/persoon.js
+++ app/models/persoon.js
@@ -2,5 +2,15 @@ import Persoon from '@lblod/ember-rdfa-editor-mandataris-plugin/models/persoon' 
 import { hasMany } from 'ember-data/relationships';
 export default Persoon.extend({
   isKandidaatVoor: hasMany('kandidatenlijst', { inverse: 'kandidaten' }),
-  verkiezingsresultaten: hasMany('verkiezingsresultaat', { inverse: null})
+  verkiezingsresultaten: hasMany('verkiezingsresultaat', { inverse: null}),
+  rdfaBindings: { // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
+    class: "http://www.w3.org/ns/person#Person",
+    achternaam: "http://xmlns.com/foaf/0.1/familyName",
+    gebruikteVoornaam: "http://data.vlaanderen.be/ns/persoon#gebruikteVoornaam",
+    alternatieveNaam: "http://xmlns.com/foaf/0.1/name",
+    geslacht: "http://data.vlaanderen.be/ns/persoon#geslacht",
+    isAangesteldAls: "http://data.vlaanderen.be/ns/mandaat#isAangesteldAls",
+    geboorte: "http://data.vlaanderen.be/ns/persoon#heeftGeboorte",
+    verkiezingsresultaten: "http://data.vlaanderen.be/ns/mandaat#isResultaatVan"
+  }
 });
