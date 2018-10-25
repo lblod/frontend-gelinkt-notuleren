0f68f74523960ac98f31d2facb7a46d178594742
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-25 15:15:53 +0200

    linting + bump

--- app/models/editor-document.js
+++ app/models/editor-document.js
@@ -1,6 +1,6 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { belongsTo, hasMany } from 'ember-data/relationships';
+import { belongsTo } from 'ember-data/relationships';
 import defaultContext from '../config/editor-document-default-context';
 
 export default Model.extend({
