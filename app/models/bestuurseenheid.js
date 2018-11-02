73c6a63f4412945b5297ab0798c7916e83f61020
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-11-02 15:50:23 +0100

    added models

--- app/models/bestuurseenheid.js
+++ app/models/bestuurseenheid.js
@@ -1,9 +1,18 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { hasMany } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   naam: attr(),
+  classificatie: belongsTo('bestuurseenheid-classificatie-code', { inverse: null }),
   bestuursorganen: hasMany('bestuursorgaan', { inverse: 'bestuurseenheid' }),
-  uri: attr()
+  uri: attr(),
+
+  rdfaBindings: { // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
+    naam: "http://www.w3.org/2004/02/skos/core#prefLabel",
+    class: "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid",
+    werkingsgebied: "http://data.vlaanderen.be/ns/besluit#werkingsgebied",
+    bestuursorgaan: "http://data.vlaanderen.be/ns/besluit#bestuurt",
+    classificatie: "http://data.vlaanderen.be/ns/besluit#classificatie"
+  }
 });
--- app/models/kandidatenlijst.js
+++ app/models/kandidatenlijst.js
@@ -1,1 +1,18 @@
-export { default } from '@lblod/ember-rdfa-editor-gemeenteraadsleden-aanstelling-plugin/models/kandidatenlijst';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  uri: attr(),
+  lijstnaam: attr(),
+  lijstnummer: attr(),
+  rechtstreekseVerkiezing: belongsTo('rechtstreekse-verkiezing', { inverse: null }),
+  kandidaten: hasMany('persoon', { inverse: 'isKandidaatVoor' }),
+  rdfaBindings: {// eslint-disable-line ember/avoid-leaking-state-in-ember-objects
+    lijstnaam: "http://www.w3.org/2004/02/skos/core#prefLabel",
+    lijstnummer: "http://data.vlaanderen.be/ns/mandaat#lijstnummer",
+    kandidaten: "http://data.vlaanderen.be/ns/mandaat#heeftKandidaat",
+    rechtstreekseVerkiezing: "http://data.vlaanderen.be/ns/mandaat#behoortTot",
+    "lijsttype": "http://data.vlaanderen.be/ns/mandaat#lijsttype"
+  }
+});
