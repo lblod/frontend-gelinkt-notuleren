7374ae6113b2d258de1120d60109b81a693016ff
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-11-26 17:30:52 +0100

    changed saving in edit mode
     - keep scroll position
     - made a task from it

--- app/controllers/editor-documents/edit.js
+++ app/controllers/editor-documents/edit.js
@@ -3,4 +3,20 @@ import EditorDocumentBaseController from '../../mixins/editor-document-base-cont
 
 export default Controller.extend(EditorDocumentBaseController, {
 
+  save: task(function *() {
+    let nearestNode = this.editor.currentNode.parentElement;
+    this.scrollToPlugin.addScrollToLocation(this.editor, nearestNode, 'last-save-position', true, false);
+     let editorDocument = this.editorDocument;
+     if(this.hasDocumentValidationErrors(editorDocument)){
+       this.set('validationErrors', true);
+       return;
+     }
+    yield this.saveEditorDocument(editorDocument);
+
+    //Scroll to is done in next runloop.
+    //Reason: basically, we have a spinner hiding everything.
+    //The domNode of editor where it should scroll to is not visible. The scrolling does not work then.
+    //I tried several different ways to make this smoother. But I couldn't
+    next(() => { this.scrollToPlugin.scrollTo('last-save-position'); });
+  }),
 });
