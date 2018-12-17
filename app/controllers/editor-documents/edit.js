486f40fa8c4981a65de986fde55fdc7188fcaf70
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-12-17 17:27:18 +0100

    update sync flow

--- app/controllers/editor-documents/edit.js
+++ app/controllers/editor-documents/edit.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import EditorDocumentBaseController from '../../mixins/editor-document-base-controller';
-import { task } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
 import ENV from 'frontend-gelinkt-notuleren/config/environment';
 import { inject as service } from '@ember/service';
 
@@ -19,12 +19,13 @@ export default Controller.extend(EditorDocumentBaseController, {
      let editorDocument = this.editorDocument;
      if(this.hasDocumentValidationErrors(editorDocument)){
        this.set('validationErrors', true);
-       return;
+       return null;
      }
 
     const savedDoc = yield this.saveEditorDocument(editorDocument);
     return savedDoc;
   }),
+
   syncDocument: task(function * () {
     const savedDoc = yield this.save.perform();
     /*
@@ -50,11 +51,21 @@ export default Controller.extend(EditorDocumentBaseController, {
     },
 
     async syncDocument() {
-      await this.syncDocument.perform();
+      this.set('syncModalDisplay', false);
+      const savedDoc = await this.syncDocument.perform();
+      this.transitionToRoute('editor-documents.edit', savedDoc.id, {queryParams: { scrollToLastSavePosition: true } });
     },
     async save(){
       const savedDoc = await this.save.perform();
       this.transitionToRoute('editor-documents.edit', savedDoc.id, {queryParams: { scrollToLastSavePosition: true } });
-   }
+    },
+
+    startSyncModal(){
+      this.set('syncModalDisplay', true);
+    },
+
+    cancelSyncModal(){
+      this.set('syncModalDisplay', false);
+    }
   }
 });
