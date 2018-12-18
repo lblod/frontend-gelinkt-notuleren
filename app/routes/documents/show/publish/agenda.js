9b69f5d3c6f2bbcfa729fd9407250ba3877ccc1b
Aad Versteden <madnificent@gmail.com>
2018-12-18 09:26:32 +0100

    Render agenda before publishing
    Renders the agenda in the publishing flow so users can see what they
    are going to sign off on.  These are the contents which come from he
    backend, generated as they will be for the blockchaing signature.

--- app/routes/documents/show/publish/agenda.js
+++ app/routes/documents/show/publish/agenda.js
@@ -1,7 +1,22 @@
+import { inject } from '@ember/service';
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  ajax: inject(),
   model(){
-    return this.modelFor('documents.show');
+    const documentContainer = this.modelFor('documents.show');
+    return RSVP.hash({
+      documentContainer: documentContainer,
+      documentIdentifier: documentContainer.id,
+      agendaContents: this.getAgendaContents( documentContainer )
+    });
+  },
+  async getAgendaContents(documentContainer) {
+    const editorDocument = await documentContainer.get('currentVersion');
+    const editorDocumentId = editorDocument.id;
+    const requestUrl = `/prepublish/agenda/${editorDocumentId}`;
+    const response = await this.ajax.request( requestUrl );
+    return response.data.attributes.content;
   }
 });
