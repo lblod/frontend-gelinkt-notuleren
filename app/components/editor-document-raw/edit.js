31b2d529e02cce15c5dc29b5115c0aeaf9ebb593
Erika Pauwels <erika.pauwels@gmail.com>
2018-12-28 14:12:00 +0100

    Fix linting errors

--- app/components/editor-document-raw/edit.js
+++ app/components/editor-document-raw/edit.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { task, timeout } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
