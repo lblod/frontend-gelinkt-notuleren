b2b6da7425c54554e110be2d674416c7ec0b62bc
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-04 13:42:02 +0100

    big commit: update raw view

--- app/components/editor-document-raw/edit.js
+++ app/components/editor-document-raw/edit.js
@@ -1,23 +1,69 @@
 import Component from '@ember/component';
-import { task } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
+import FileSaverMixin from 'ember-cli-file-saver/mixins/file-saver';
 
-export default Component.extend({
+export default Component.extend(FileSaverMixin, {
   router: service(),
+
+  isWorking: computed('updateDocument.isRunning', 'uploadFileAndReplace.isRunning', 'saveDocument.isRunning', function(){
+    return this.saveDocument.isRunning || this.uploadFileAndReplace.isRunning || this.saveDocument.isRunning;
+  }),
 
   outputId: computed('id', function() {
      return `output-editor-document-${this.elementId}`;
    }),
 
-  saveDocument: task(function * () {
+  updateDocument: task(function *() {
     this.document.set('content', document.getElementById(this.outputId).textContent);
     yield this.document.save();
   }),
 
+  saveDocument: task(function *() {
+    yield this.document.save();
+  }),
+
+  //hack hack
+  uploadFileAndReplace: task(function * (file) {
+    let reader = new FileReader();
+    yield new Promise(resolve => {
+      let content = '';
+      reader.onload = event => {
+        content = event.target.result;
+        console.log(event.target.result);
+        this.document.set('content', content);
+        resolve();
+      };
+      reader.readAsText(file);
+    });
+  }),
+
   actions: {
+
+    toggleFileUpload(){
+      this.toggleProperty('uploadFileMode');
+    },
+
+    //hack hack
+    uploadFileAndReplace(uploadEvent){
+      const file = uploadEvent.target.files[0];
+      this.uploadFileAndReplace.perform(file);
+    },
+
+    generateFileDownload(){
+       this.saveFileAs(`raw_document_content_${this.document.id}.txt`, this.document.content, 'text/plain');
+    },
+
+    toggleDisplayRaw(){
+      this.toggleProperty('displayRaw');
+    },
     save(){
       this.saveDocument.perform();
+    },
+
+    update(){
+      this.updateDocument.perform();
     },
     next(){
       this.router.transitionTo('editor-documents.raw', this.document.nextVersion);
