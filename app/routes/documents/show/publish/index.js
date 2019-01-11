7a31a26a9c7affe68c377422994ac22c6bff31a5
Miet Claes <miet@miet.be>
2019-01-11 09:15:04 +0100

    Change default screen to agenda

--- app/routes/documents/show/publish/index.js
+++ app/routes/documents/show/publish/index.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   activate(){
-    this.transitionTo('documents.show.publish.overview');
+    this.transitionTo('documents.show.publish.agenda');
   }
 });
