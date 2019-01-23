201c56492247f5d14419866c6b1fc1b8ee7368d5
Miet Claes <miet@miet.be>
2019-01-23 08:27:59 +0100

    Make labels and buttons more consistent

--- app/components/document-link-to-published.js
+++ app/components/document-link-to-published.js
@@ -23,18 +23,18 @@ export default Component.extend({
 
     switch(id) {
       case "c272d47d756d4aeaa0be72081f1389c6":
-        // ondertekende notulen gepubliceerd
-        return "ga naar gepubliceerde en ondertekende notulen";
+        // notulen gepubliceerd
+        return "Bekijk&nbsp;publieke&nbsp;notulen";
       case "cfd751588a6c453296de9f9c0dff2af4":
       case "5A8304E8C093B00009000010":
         // concept
         return null;
       case "627aec5d144c422bbd1077022c9b45d1":
         // agenda publiek
-        return "Bekijk&nbsp;gepubliceerde&nbsp;agenda";
+        return "Bekijk&nbsp;publieke&nbsp;agenda";
       case "b763390a63d548bb977fb4804293084a":
-        // notulen publiek
-        return "ga naar publieke notulen";
+        // besluitenlijst publiek
+        return "Bekijk&nbsp;publieke&nbsp;besluitenlijst";
     }
     return null;
   }),
