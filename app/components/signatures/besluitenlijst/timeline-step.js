4b0c4923afe8b04605f771abeada8011b55528c4
Niels Vandekeybus <progster@gmail.com>
2019-02-08 16:36:40 +0100

    initial support for besluitenlijst

---
+++ app/components/signatures/besluitenlijst/timeline-step.js
@@ -0,0 +1,36 @@
+import RSVP from 'rsvp';
+import { get } from '@ember/object';
+import { inject } from '@ember/service';
+import { computed } from '@ember/object';
+import Component from '@ember/component';
+import PromiseProxyObject from 'frontend-gelinkt-notuleren/utils/promise-proxy-object';
+
+export default Component.extend({
+  tagName: '',
+  ajax: inject(),
+
+  // This is an agenda object proxy onto which we dump a bunch of
+  // contents necessary in the template.  Our construction works this
+  // way to keep the template somewhat cleaner.
+  mockLijst: computed('lijst', 'currentEditorDocument', function(){
+    if( this.agenda ) {
+      // pick all info from the current agenda
+      return PromiseProxyObject.create( {
+        promise: RSVP.hash( {
+          body: this.lijst.content,
+          signedId: this.lijst.editorDocument.then( (ed) => ed.id )
+        } )
+      } );
+    } else {
+      // create an agenda with dumped contents and put it in a promise proxy
+      return PromiseProxyObject.create({
+        promise: RSVP.hash( {
+          body: this.ajax
+            .request(`/prepublish/besluitenlijst/${this.currentEditorDocument.id}`)
+            .then( (response) => get(response, "data.attributes.content") ),
+          signedId: get( this, 'currentEditorDocument.id' )
+        })
+      });
+    }
+  })
+});
--- app/routes/documents/show/publish/besluitenlijst.js
+++ app/routes/documents/show/publish/besluitenlijst.js
@@ -1,4 +1,19 @@
+import { inject } from '@ember/service';
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  ajax: inject(),
+  model(){
+    const documentContainer = this.modelFor('documents.show');
+    return RSVP.hash({
+      documentContainer: documentContainer,
+      documentIdentifier: documentContainer.id,
+      editorDocument: this.getEditorDocument( documentContainer )
+    });
+  },
+  async getEditorDocument( documentContainer ) {
+    const editorDocument = await documentContainer.get('currentVersion');
+    return editorDocument;
+  }
 });
