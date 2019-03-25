0044d8410a93fc2a3b256f855ffb058b21926c6a
Miet Claes <miet@miet.be>
2019-03-25 14:00:50 +0100

    Make sure data for users is available in the publication and signing flow

--- app/components/signatures/publication-confirmation.js
+++ app/components/signatures/publication-confirmation.js
@@ -1,8 +1,10 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   tagName: '',
 
+  currentSession: service(),
   name: null,
   mockDocument: null,
   confirm: null,
