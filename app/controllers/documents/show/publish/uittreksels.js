5f0e5867c30114ee358566a63280475cecf816e5
Niels Vandekeybus <progster@gmail.com>
2019-03-25 21:40:17 +0100

    support signing and publishing behandelingen

--- app/controllers/documents/show/publish/uittreksels.js
+++ app/controllers/documents/show/publish/uittreksels.js
@@ -1,19 +1,32 @@
+import RSVP from 'rsvp';
 import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import PromiseProxyObject from 'frontend-gelinkt-notuleren/utils/promise-proxy-object';
 
 export default Controller.extend({
   documentContainer: alias('model.documentContainer'),
   documentIdentifier: alias('model.documentIdentifier'),
   currentEditorDocument: alias('model.editorDocument'),
   ajax: service(),
-  behandelingen: computed('model.behandelingen.data', function() {
+  behandelingen: computed('model.behandelingen.data.@each.uuid', function() {
     return this.get('model.behandelingen.data').map((b) => {
       const attr = b.attributes;
       attr.signedId = this.currentEditorDocument.id;
-      attr.encodedUri = encodeUriComponent(attr.behandeling);
-      return attr;
+      attr.uri = attr.behandeling;
+      if (attr.uuid) {
+        // todo: not very clean, sorry
+        attr.realBehandeling = this.store.find('versioned-behandeling', attr.uuid);
+        return PromiseProxyObject.create( {
+          promise: RSVP.hash(attr)
+        });
+      }
+      else {
+        return PromiseProxyObject.create( {
+          promise: RSVP.hash(attr)
+        });
+      }
     });
   }),
   actions: {
@@ -26,14 +39,15 @@ export default Controller.extend({
      * agenda but haven't implemented this so far.
      */
     async applySignature(behandeling, documentId) {
-      await this.ajax.post('/signing/behandeling/sign/${documentId}/${behandeling}');
-
+      const encodedUri = encodeURIComponent(encodeURIComponent(behandeling)); // TODO need to double encode, figure out why
+      await this.ajax.post(`/signing/behandeling/sign/${documentId}/${encodedUri}`);
     },
     /**
      * Publishes the document.
      */
     async publish(behandeling, documentId) {
-      await this.ajax.post('/signing/behandeling/publish/${documentId}/${behandeling}');
+      const encodedUri = encodeURIComponent(encodeURIComponent(behandeling)); // TODO need to double encode, figure out why
+      await this.ajax.post(`/signing/behandeling/publish/${documentId}/${encodedUri}`);
     }
   }
 
