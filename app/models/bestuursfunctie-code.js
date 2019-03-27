8a569e072b99145fe82ea8991840a719564e75d9
Claire Lovisa <lovisa.claire@gmail.com>
2019-03-27 17:12:05 +0100

    Adapt model to default mandate types per bestuursorgaan

--- app/models/bestuursfunctie-code.js
+++ app/models/bestuursfunctie-code.js
@@ -1,10 +1,13 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   label: attr(),
   scopeNote: attr(),
   uri: attr(),
+  standaardTypeVan: hasMany('bestuursorgaan-classificatie-code', { inverse: 'standaardType' }),
+
   rdfaBindings: { // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
     class: "http://www.w3.org/2004/02/skos/core#Concept",
     label: "http://www.w3.org/2004/02/skos/core#prefLabel"
