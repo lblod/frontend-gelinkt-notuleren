8cd45ae7ea0b13b446d40e1e2dd100dc7f400580
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-05-07 16:59:37 +0200

    remove last scroll position save

--- app/controllers/editor-documents/new.js
+++ app/controllers/editor-documents/new.js
@@ -11,7 +11,6 @@ export default Controller.extend(EditorDocumentBaseController, {
 
   saveAndTransitionToEditMode: task(function * (){
     let nearestNode = this.editor.currentNode.parentElement;
-    this.scrollToPlugin.addScrollToLocation(this.editor, nearestNode, 'last-save-position', true, false);
      let editorDocument = this.editorDocument;
      if(this.hasDocumentValidationErrors(editorDocument)){
        this.set('validationErrors', true);
@@ -19,7 +18,7 @@ export default Controller.extend(EditorDocumentBaseController, {
      }
     let savedDoc = yield this.saveEditorDocument.perform(editorDocument, this.getStatusFor('conceptStatusId'));
     const container = yield savedDoc.get('documentContainer');
-    this.transitionToRoute('editor-documents.edit', container.id, {queryParams: { scrollToLastSavePosition: true } });
+    this.transitionToRoute('editor-documents.edit', container.id);
   }),
 
   actions: {
