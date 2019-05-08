99401d58bf5d52f79c049bc533697cc2fc21262a
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-05-08 11:59:33 +0200

    Bugfix: workaround. Component should not rerender after save.

--- app/controllers/editor-documents/new.js
+++ app/controllers/editor-documents/new.js
@@ -1,13 +1,19 @@
 import Controller from '@ember/controller';
 import EditorDocumentBaseController from '../../mixins/editor-document-base-controller';
-import { task } from 'ember-concurrency';
+import { task, waitForProperty } from 'ember-concurrency';
 import ENV from 'frontend-gelinkt-notuleren/config/environment';
+import { warn } from '@ember/debug';
+import { computed } from '@ember/object';
 
 export default Controller.extend(EditorDocumentBaseController, {
   init() {
     this._super(...arguments);
     this.set('publicationUrl', ENV['publicatie']['baseUrl']);
   },
+
+  hasPerformedSaveAndTransitionToEditModeTask: computed('isTransitioningToEditRoute.performCount', function(){
+    return this.saveAndTransitionToEditMode.performCount > 0;
+  }),
 
   saveAndTransitionToEditMode: task(function * (){
     let nearestNode = this.editor.currentNode.parentElement;
