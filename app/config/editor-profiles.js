89d5602ff66ae211e8583f19cbf8f9d332491c31
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-07-31 14:04:28 +0200

    remove obsolete plugins

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -1,12 +1,8 @@
 export default {
   default: [
-    "rdfa-editor-schepenen-aanstelling-plugin",
-    "rdfa-editor-installatievergadering-plugin",
     "rdfa-editor-stemming-module-plugin",
     "rdfa-editor-aanwezigen-plugin",
-    "rdfa-editor-fractievorming-plugin",
     "rdfa-editor-scroll-to-plugin",
-    "rdfa-editor-gemeenteraadsleden-aanstelling-plugin",
     "rdfa-editor-scoped-bestuursorgaan-plugin",
     "rdfa-editor-document-tasklist-plugin",
     "rdfa-editor-template-variables-manager-plugin",
@@ -24,13 +20,9 @@ export default {
 
   //GEMEENTE
   "http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/5ab0e9b8a3b2ca7c5e000001": [
-//    "rdfa-editor-schepenen-aanstelling-plugin",
-    "rdfa-editor-installatievergadering-plugin",
     "rdfa-editor-stemming-module-plugin",
     "rdfa-editor-aanwezigen-plugin",
-    "rdfa-editor-fractievorming-plugin",
     "rdfa-editor-scroll-to-plugin",
-    "rdfa-editor-gemeenteraadsleden-aanstelling-plugin",
     "rdfa-editor-scoped-bestuursorgaan-plugin",
     "rdfa-editor-document-tasklist-plugin",
     "rdfa-editor-template-variables-manager-plugin",
@@ -48,8 +40,6 @@ export default {
 
   //OCMW
   "http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/5ab0e9b8a3b2ca7c5e000002": [
-    "rdfa-editor-bijzonder-comite-aanstelling-plugin",
-    "rdfa-editor-installatievergadering-plugin",
     "rdfa-editor-stemming-module-plugin",
     "rdfa-editor-aanwezigen-plugin",
     "rdfa-editor-scroll-to-plugin",
