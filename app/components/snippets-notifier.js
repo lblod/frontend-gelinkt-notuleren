e700286dfef61fc0f4c9ca0b0da98a8662c6257d
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-09-13 12:04:51 +0200

    added some display card logic

--- app/components/snippets-notifier.js
+++ app/components/snippets-notifier.js
@@ -3,6 +3,11 @@ import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+  show: true,
+  showCard: computed('hasSnippets', 'show', function(){
+    return this.hasSnippets && this.show;
+  }),
+
   importRdfaSnippet: service('import-rdfa-snippet'),
   snippets: computed('importRdfaSnippet.snippets.[]', function(){
     return this.get('importRdfaSnippet.snippets.[]');
@@ -11,5 +16,11 @@ export default Component.extend({
     return this.get('importRdfaSnippet.errors.[]');
   }),
   hasSnippets: computed('snippets.[]', function(){ return this.snippets.length > 0; }),
-  hasErrors: computed('errors.[]', function(){ return this.errors.length > 0; })
+  hasErrors: computed('errors.[]', function(){ return this.errors.length > 0; }),
+
+  actions: {
+    close(){
+      this.set('show', false);
+    }
+  }
 });
