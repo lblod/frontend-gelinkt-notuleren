cadc47bb39ca2d1e5f2a2284a47dffc77d01e332
Dieter Peirs <github@dieterpeirs.com>
2020-02-26 13:21:17 +0100

    add header to the login page

--- app/components/vo-page.js
+++ app/components/vo-page.js
@@ -3,7 +3,7 @@ import ENV from 'frontend-gelinkt-notuleren/config/environment';
 export default Component.extend({
   init() {
     this._super(...arguments);
-    this.set('header', ENV['vo-webuniversum']['header']);
+    // this.set('header', ENV['vo-webuniversum']['header']);
     this.set('footer', ENV['vo-webuniversum']['footer']);
     this.set('showFooter', true);
   }
