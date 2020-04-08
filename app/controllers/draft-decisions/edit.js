2034fddf864c20ee83ec82b38258f36b1f8edfe5
Claire Lovisa <lovisa.claire@gmail.com>
2020-04-08 13:11:59 +0200

    Clean non-used code

--- app/controllers/draft-decisions/edit.js
+++ app/controllers/draft-decisions/edit.js
@@ -17,28 +17,14 @@ export default Controller.extend(EditorDocumentBaseController, {
     return savedDoc;
   }),
 
-  exportHtml: task(function * () {
-    // TODO
-    return null;
-  }),
-
   actions: {
     handleRdfaEditorInit(editor){
       this.set('editor', editor);
     },
 
-    async exportHtml() {
-      this.set('exportHtmlDisplay', false);
-      await this.exportHtml.perform();
-    },
-
     async save() {
       const savedDoc = await this.save.perform();
       this.set('editorDocument', savedDoc);
-    },
-
-    exportHtmlModal() {
-      this.set('exportHtmlDisplay', true);
     },
 
     cancelSyncModal() {
