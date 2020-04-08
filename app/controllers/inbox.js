99f1c544ac9f8da4efa33aefa54c901b76265ae7
Claire Lovisa <lovisa.claire@gmail.com>
2020-04-08 11:14:22 +0200

    Rework new zitting button's place

--- app/controllers/inbox.js
+++ app/controllers/inbox.js
@@ -2,11 +2,5 @@ import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  currentSession: service(),
-
-  actions: {
-    openNewDocument() {
-      this.transitionToRoute('editor-documents.new');
-    }
-  }
+  currentSession: service()
 });
