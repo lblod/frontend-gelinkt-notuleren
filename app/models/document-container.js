053d23b731768b0a6395d7f9143ffc80b2993aad
Claire Lovisa <lovisa.claire@gmail.com>
2020-04-10 10:12:58 +0200

    Add an other type to the draft decision document

--- app/models/document-container.js
+++ app/models/document-container.js
@@ -4,9 +4,9 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 export default Model.extend({
   revisions: hasMany( 'editor-document', { inverse: 'documentContainer' } ),
   currentVersion: belongsTo( 'editor-document', { inverse: null } ),
-  status: belongsTo('editor-document-status', {inverse: null }),
-  folder: belongsTo('editor-document-folder', {inverse: null }),
-  publisher: belongsTo('bestuurseenheid', {inverse: null }),
+  status: belongsTo('editor-document-status', { inverse: null }),
+  folder: belongsTo('editor-document-folder', { inverse: null }),
+  publisher: belongsTo('bestuurseenheid', { inverse: null }),
   versionedAgendas: hasMany('versioned-agenda'),
   versionedNotulen: hasMany('versioned-notulen'),
   versionedBesluitenLijsten: hasMany('versioned-besluiten-lijst'),
