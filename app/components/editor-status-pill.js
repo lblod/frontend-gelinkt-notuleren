8689cddf60c32520f3630d63b49ba5396d4963a8
Dieter Peirs <github@dieterpeirs.com>
2020-05-12 15:28:57 +0200

    replace pill component

--- app/components/editor-status-pill.js
+++ app/components/editor-status-pill.js
@@ -8,18 +8,18 @@ export default Component.extend({
     switch(id) {
     case "c272d47d756d4aeaa0be72081f1389c6":
       // ondertekende notulen gepubliceerd
-      return "pill--gray-dark";
+      return "success";
     case "cfd751588a6c453296de9f9c0dff2af4":
     case "5A8304E8C093B00009000010":
       // concept
       return null;
     case "627aec5d144c422bbd1077022c9b45d1":
       // agenda publiek
-      return "pill--primary-yellow";
+      return "warning";
     case "b763390a63d548bb977fb4804293084a":
       // besluitenlijst publiek
-      return "pill--primary-yellow";
+      return "warning";
     }
-    return "pill--primary-blue";
+    return "";
   })
 });
