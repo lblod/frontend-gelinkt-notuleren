06d1fb23131a365b5937a3c20438f5eeb8f71d2d
Niels Vandekeybus <progster@gmail.com>
2020-06-16 15:38:28 +0200

    fall back to dutch if no locale is provided

--- app/routes/application.js
+++ app/routes/application.js
@@ -15,8 +15,8 @@ export default Route.extend(ApplicationRouteMixin, {
     flatpickr.localize(flatpickr.l10ns.nl);
     this.moment.setTimeZone('Europe/Brussels');
     this.moment.setLocale('nl');
-    const userLocale = ( navigator.language || navigator.languages[0] )
-    this.intl.setLocale([userLocale, 'en-us']);
+    const userLocale = ( navigator.language || navigator.languages[0] );
+    this.intl.setLocale([userLocale, 'nl-be']);
     this.updateFeatureFlags(transition.to.queryParams);
     return this.loadCurrentSession();
   },
