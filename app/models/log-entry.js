8507af381a5a4dc901821f1a5931b731a64cbba0
Oscar Rodriguez <oscar@verify.as>
2020-06-30 14:32:58 +0200

    added models needed for saving errors to the backend

---
+++ app/models/log-entry.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default DS.Model.extend({
+  className: attr(),
+  message: attr(),
+  specificInformation: attr(),
+  datetime: attr(),
+  logSource: belongsTo('log-source', { inverse: null }),
+  logLevel: belongsTo('log-level', { inverse: null }),
+  statusCode: belongsTo('status-code', { inverse: null })
+});
