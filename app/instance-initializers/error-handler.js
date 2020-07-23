89ea96bef6548623e5f5982a8c029e540b000fe2
Oscar Rodriguez <oscar@verify.as>
2020-07-23 12:32:36 +0200

    added try catch to be a little bit more defensive

--- app/instance-initializers/error-handler.js
+++ app/instance-initializers/error-handler.js
@@ -2,17 +2,22 @@ export function initialize(appInstance) {
   const store = appInstance.lookup('service:store');
   
   window.onerror = async function (msg, url, lineNo, columnNo, error) {
-    const logSource = await store.findRecord('log-source', 'F400FB90-81F6-4F49-B4F5-CDFAD0DA1243');
-    const logLevel = await store.findRecord('log-level', '3af9ebe1-e6a8-495c-a392-16ced1f38ef1');
-    const logEntry = store.createRecord('log-entry', {
-      message: msg,
-      specificInformation: error.stack,
-      datetime: new Date(),
-      logSource: logSource,
-      resource: location.href,
-      logLevel: logLevel,
-    });
-    await logEntry.save();
+    try {
+      const logSource = await store.findRecord('log-source', 'F400FB90-81F6-4F49-B4F5-CDFAD0DA1243');
+      const logLevel = await store.findRecord('log-level', '3af9ebe1-e6a8-495c-a392-16ced1f38ef1');
+      const logEntry = store.createRecord('log-entry', {
+        message: msg,
+        specificInformation: error.stack,
+        datetime: new Date(),
+        logSource: logSource,
+        resource: location.href,
+        logLevel: logLevel,
+      });
+      await logEntry.save();
+    }catch(error) {
+      console.log('Error while trying to log the error to the backend');
+      console.log(error);
+    }
   };
   
   return false;
