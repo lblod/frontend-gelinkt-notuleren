eafb3184ce4244485ec26cc96ae9899b0c196bb1
Oscar <lagartoverde97@gmail.com>
2020-09-23 15:51:06 +0200

    improved the bestuursorgan selector

--- app/components/meeting-form.js
+++ app/components/meeting-form.js
@@ -12,6 +12,25 @@ export default class MeetingForm extends Component{
   @tracked bestuursorgaanOptions;
 
   @service store;
+  @service currentSession;
+
+  allowedBestuursorgaanClassifications = [ 
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e000005", //	"Gemeenteraad"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e000007", //	"Raad voor Maatschappelijk Welzijn"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e000009", //	"Bijzonder Comité voor de Sociale Dienst"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e00000a", //	"Districtsraad"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e00000c", //	"Provincieraad"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/53c0d8cd-f3a2-411d-bece-4bd83ae2bbc9", //	"Voorzitter van het Bijzonder Comité voor de Sociale Dienst"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/9314533e-891f-4d84-a492-0338af104065", //	"Districtsburgemeester"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e00000b", //	"Districtscollege"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/180a2fba-6ca9-4766-9b94-82006bb9c709", //	"Gouverneur"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/e14fe683-e061-44a2-b7c8-e10cab4e6ed9", //	"Voorzitter van de Raad voor Maatschappelijk Welzijn"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e000006", //	"College van Burgemeester en Schepenen"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/4c38734d-2cc1-4d33-b792-0bd493ae9fc2", //	"Voorzitter van de Gemeenteraad"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e00000d", //	"Deputatie"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/4955bd72cd0e4eb895fdbfab08da0284", //	"Burgemeester"
+    "http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e000008" //	"Vast Bureau"
+    ]
 
   constructor() {
     super(...arguments);
@@ -25,7 +44,17 @@ export default class MeetingForm extends Component{
   }
 
   async fetchBestuursorgaan() {
-    this.bestuursorgaanOptions = await this.store.findAll('bestuursorgaan');
+    // TODO: Move select Bestuursorgaan to component
+    const currentBestuurseenheid = await this.currentSession.get('group');
+    let query = {
+      'filter[is-tijdsspecialisatie-van][bestuurseenheid][id]': currentBestuurseenheid.id,
+      'include':'is-tijdsspecialisatie-van,is-tijdsspecialisatie-van.bestuurseenheid,is-tijdsspecialisatie-van.classificatie',
+      'sort': '-binding-start'
+    };
+    let bestuursorganenInTijd = await this.store.query('bestuursorgaan', query);
+    bestuursorganenInTijd = bestuursorganenInTijd.filter(b => this.allowedBestuursorgaanClassifications.includes(b.get('isTijdsspecialisatieVan.classificatie.uri')));
+    console.log(bestuursorganenInTijd)
+    this.bestuursorgaanOptions = bestuursorganenInTijd
   }
 
   @action
@@ -34,8 +63,8 @@ export default class MeetingForm extends Component{
   }
 
   @action
-  changeDate(property, value) {
-    this[property] = value;
+  changeDate(targetProperty, value) {
+    this[targetProperty] = value;
   }
 
   @action
