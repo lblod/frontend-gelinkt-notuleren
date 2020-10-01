1b6f8c6d4f5f4ebe5c86a1f23c4dde1bddfafd22
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-01 19:40:24 +0200

    fix: due to bad merge fix

--- app/routes/meetings/edit.js
+++ app/routes/meetings/edit.js
@@ -3,7 +3,10 @@ import Route from '@ember/routing/route';
 export default class MeetingsEditRoute extends Route {
 
   async model(params) {
-    const zitting = await this.store.findRecord('zitting', params.id, { include: 'aanwezigen-bij-start,agendapunt' }); //include omits pagination
+    const zitting = await this.store.findRecord('zitting', params.id,
+                                                {
+                                                  include: 'aanwezigen-bij-start,agendapunten'
+                                                });
     zitting.agendapunten = zitting.agendapunten.sortBy('position');
     return zitting;
   }
