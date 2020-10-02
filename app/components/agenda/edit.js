8c77931bd7169d019bc9ca62423ff4a6fb56e9ec
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-02 15:03:34 +0200

    a tad more elegant editMode.
    - Before when saved, you could see the 'delete-button' popping up
    whilst saving. Not elegant. Now the state of create mode remains

--- app/components/agenda/edit.js
+++ app/components/agenda/edit.js
@@ -4,6 +4,13 @@ import { action } from '@ember/object';
 
 
 export default class AgendaEditComponent extends Component {
+  @tracked isEditMode = false;
+
+  constructor(){
+    super(...arguments);
+    this.isEditMode = this.args.agendapunt.id ? true : false;
+  }
+
   @action
   async toggleGeplandOpenbaar(){
     this.args.agendapunt.geplandOpenbaar=!this.args.agendapunt.geplandOpenbaar;
