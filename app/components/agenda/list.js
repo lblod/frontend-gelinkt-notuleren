24c90a1418b17161f1967173794ad19d509644d1
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-02 09:23:16 +0200

    some comments and to do on the sorting

--- app/components/agenda/list.js
+++ app/components/agenda/list.js
@@ -6,12 +6,16 @@ export default class AgendaListComponent extends Component {
     super(...args);
   }
 
-
+  //TODO: make sure the behandeltNa property is properly set
+  //TODO 2: make this into a task as it will be slow for many agendapoints. So you can add a spinner the user knows he has to wait
+  // Note agendas with more than 50 points are super common!
+  //TODO: no async for each please.
+  //TODO: i am not sure to undertand the logic (the index == new position so?)
   @action
   async sort(){
     this.args.agendapunten.forEach(async function(agendapunt, index){
-      var position=this.args.agendapunten.indexOf(agendapunt);
-      this.args.agendapunten.objectAt(index).position=position;
+      const position = this.args.agendapunten.indexOf(agendapunt);
+      this.args.agendapunten.objectAt(index).position = position;
       await this.args.agendapunten.objectAt(index).save();
     }.bind(this));
   }
