755fdaf44b01b0a2c4b66d525405e438857cef06
abeforgit <arnebertrand@gmail.com>
2020-10-06 17:08:44 +0200

    Add voting component

---
+++ app/components/voting/modal.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+
+/**
+ * @typedef {import("../../models/behandeling-van-agendapunt").default} Behandeling
+ */
+
+/**
+ * @typedef {Object} Args
+ * @property {Behandeling} behandeling
+ */
+
+/** @extends {Component<Args>} */
+export default class VotingModalComponent extends Component {
+
+}
--- app/models/behandeling-van-agendapunt.js
+++ app/models/behandeling-van-agendapunt.js
@@ -2,15 +2,15 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
-export default Model.extend({
-  openbaar: attr(),
-  afgeleidUit: attr(),
-  gevolg: attr('language-string'),
-  vorigeBehandelingVanAgendapunt: belongsTo('behandeling-van-agendapunt', {inverse: null }),
-  onderwerp: belongsTo('agendapunt', {inverse: 'behandeling' }),
-  secretaris: belongsTo('mandataris', {inverse: null }),
-  voorzitter: belongsTo('mandataris', {inverse: null }),
-  besluiten: hasMany('besluit', {inverse: 'volgendUitBehandelingVanAgendapunt'}),
-  aanwezigen: hasMany('mandataris', {inverse: null }),
+export default class BehandelingVanAgendaPunt extends Model {
+  openbaar = attr();
+  afgeleidUit = attr();
+  gevolg = attr('language-string');
+  vorigeBehandelingVanAgendapunt = belongsTo('behandeling-van-agendapunt', { inverse: null });
+  onderwerp = belongsTo('agendapunt', { inverse: 'behandeling' });
+  secretaris = belongsTo('mandataris', { inverse: null });
+  voorzitter = belongsTo('mandataris', { inverse: null });
+  besluiten = hasMany('besluit', { inverse: 'volgendUitBehandelingVanAgendapunt' });
+  aanwezigen = hasMany('mandataris', { inverse: null });
   // stemmingen: hasMany('stemming', {inverse: null }),
-});
+}
