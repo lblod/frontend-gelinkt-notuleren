0ab63541777981408c4e136bd3a465f08332e71a
abeforgit <arnebertrand@gmail.com>
2020-10-08 00:41:08 +0200

    Add a participation selector to the behandeling component

--- app/models/behandeling-van-agendapunt.js
+++ app/models/behandeling-van-agendapunt.js
@@ -2,15 +2,15 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
-export default class BehandelingVanAgendaPunt extends Model {
-  openbaar = attr();
-  afgeleidUit = attr();
-  gevolg = attr('language-string');
-  vorigeBehandelingVanAgendapunt = belongsTo('behandeling-van-agendapunt', { inverse: null });
-  onderwerp = belongsTo('agendapunt', { inverse: 'behandeling' });
-  secretaris = belongsTo('mandataris', { inverse: null });
-  voorzitter = belongsTo('mandataris', { inverse: null });
-  besluiten = hasMany('besluit', { inverse: 'volgendUitBehandelingVanAgendapunt' });
-  aanwezigen = hasMany('mandataris', { inverse: null });
+export default class BehandelingVanAgendapunt extends Model {
+  @attr openbaar;
+  @attr afgeleidUit;
+  @attr('language-string') gevolg;
+  @belongsTo('behandeling-van-agendapunt', { inverse: null }) vorigeBehandelingVanAgendapunt;
+  @belongsTo('agendapunt', { inverse: 'behandeling' }) onderwerp;
+  @belongsTo('mandataris', { inverse: null }) secretaris;
+  @belongsTo('mandataris', { inverse: null }) voorzitter;
+  @hasMany('besluit', { inverse: 'volgendUitBehandelingVanAgendapunt' }) besluiten;
+  @hasMany('mandataris', { inverse: null }) aanwezigen;
   // stemmingen: hasMany('stemming', {inverse: null }),
 }
