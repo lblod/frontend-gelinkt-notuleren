d5f1221e5ea05bb3968acd928690dc7a2d965c44
Oscar <lagartoverde97@gmail.com>
2020-10-07 10:30:06 +0200

    trying to integrate the app universum card

--- app/components/besluit-document-container.js
+++ app/components/besluit-document-container.js
@@ -1,4 +1,21 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from "@ember/object";
 
 export default class BesluitDocumentContainerComponent extends Component {
+  @service store;
+  profile = 'default'
+  constructor() {
+    super(...arguments);
+    this.document = this.store.createRecord('editor-document');
+  }
+  @action
+  handleRdfaEditorInit() {
+
+  }
+  @action
+  updateTasklists() {
+    
+  }
+
 }
