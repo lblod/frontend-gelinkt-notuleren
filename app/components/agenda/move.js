42f9467383f7cefc8516605396c2771aef35d989
Sergey Andreev <sergey.andreev@redpencil.io>
2020-10-15 14:52:20 +0200

    most ui elements implemented

--- app/components/agenda/move.js
+++ app/components/agenda/move.js
@@ -10,9 +10,41 @@ export default class AgendaMoveComponent extends Component {
     { code: 'after', name: 'Na agendapunt' },
     { code: 'end', name: 'Achteraan in agenda' }
   ];
+
+
+  @tracked showAfterAgendapuntOptions=false;
+
+  @tracked afterAgendapuntOptions=this.args.agendapunten.filter(function(agendapunt) {
+    return agendapunt.id != this.args.agendapunt.id;
+  }.bind(this));
+
+  @tracked selectedAfterAgendapunt;
+
+  @action
+  selectAfterAgendapunt(option){
+    this.selectedAfterAgendapunt=option;
+  }
+
   @action
   selectLocation(option){
     this.selectedLocation=option;
+    this.showAfterAgendapuntOptions=false;
 
+    const index=this.args.agendapunten.indexOf(this.args.agendapunt);
+
+    if(option.code=='start'){
+
+    }
+    else if(option.code=='end'){
+
+    }
+    else if(option.code=='after'){
+      this.selectedAfterAgendapunt=
+        index==0?
+          null:
+          this.args.agendapunten.objectAt(index-1);
+
+      this.showAfterAgendapuntOptions=true;
+    }
   }
 }
