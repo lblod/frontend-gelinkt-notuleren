3ad8da5deca90f9f25b709a5426c85831334403b
abeforgit <arnebertrand@gmail.com>
2020-10-19 18:47:28 +0200

    Refactor duplicate code

--- app/controllers/meetings/publish/agenda.js
+++ app/controllers/meetings/publish/agenda.js
@@ -40,62 +40,39 @@ export default class MeetingsPublishAgendaController extends Controller {
 
   @task
   * initializeAgendas() {
-    const ontwerp = yield this.store.query("agenda",
+    this.ontwerpAgenda = yield this.initializeAgenda.perform("ontwerpagenda");
+    this.aanvullendeAgenda = yield this.initializeAgenda.perform("aanvullendeagenda");
+    this.spoedeisendeAgenda = yield this.initializeAgenda.perform("spoedeisendeagenda");
+  }
+
+  /**
+   * @this {MeetingsPublishAgendaController}
+   * @param {string} type
+   * @return {Generator<*, void, *>}
+   */
+  @task
+  *initializeAgenda(type) {
+    const agendas = yield this.store.query("agenda",
       {
         'filter[zitting][:id:]': this.model.id,
-        'filter[agenda-type]': "ontwerpagenda",
+        'filter[agenda-type]': type,
         include: "signed-resources,published-resource"
       }
     );
-    if (ontwerp.length) {
-      this.ontwerpAgenda = ontwerp.firstObject;
+    if (agendas.length) {
+      return agendas.firstObject;
     } else {
       const prePublish = yield this.createPrePublishedResource.perform();
-      this.ontwerpAgenda = yield this.store.createRecord("agenda", {
-        agendaType: "ontwerpagenda",
+      const rslt = yield this.store.createRecord("agenda", {
+        agendaType: type,
         zitting: this.model,
         agendapunten: this.model.behandeldeAgendapunten,
         renderedContent: prePublish
       });
+      return rslt;
     }
-    const aanvullend = yield this.store.query("agenda",
-      {
-        'filter[zitting][:id:]': this.model.id,
-        'filter[agenda-type]': "aanvullendeagenda",
-        include: "signed-resources,published-resource"
-      }
-    );
-    console.log("AANVULLENDE", aanvullend);
-    if (aanvullend.length) {
-      this.aanvullendeAgenda = aanvullend.firstObject;
-    } else {
-      const prePublish = yield this.createPrePublishedResource.perform();
-      this.aanvullendeAgenda = yield this.store.createRecord("agenda", {
-        agendaType: "aanvullendeagenda",
-        zitting: this.model,
-        agendapunten: this.model.behandeldeAgendapunten,
-        renderedContent: prePublish
-      });
-    }
-    const spoedeisend = yield this.store.query("agenda",
-      {
-        'filter[zitting][:id:]': this.model.id,
-        'filter[agenda-type]': "spoedeisendeagenda",
-        include: "signed-resources,published-resource"
-      }
-    );
-    console.log("SPOEDEISEND", spoedeisend);
-    if (spoedeisend.length) {
-      this.spoedeisendeAgenda = spoedeisend.firstObject();
-    } else {
-      const prePublish = yield this.createPrePublishedResource.perform();
-      this.spoedeisendeAgenda = yield this.store.createRecord("agenda", {
-        agendaType: "spoedeisendeagenda",
-        zitting: this.model,
-        agendapunten: this.model.behandeldeAgendapunten,
-        renderedContent: prePublish
-      });
-    }
+
+
   }
 
   @task
@@ -104,7 +81,6 @@ export default class MeetingsPublishAgendaController extends Controller {
     const response = yield this.ajax.request(
       `/prepublish/agenda/${id}`
     );
-    console.log(response);
     return response.data.attributes.content;
   }
 
