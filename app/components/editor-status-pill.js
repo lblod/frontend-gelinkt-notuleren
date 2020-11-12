bba32f53f25f56204b517e9597baf371c792d82e
Sergey Andreev <sergey.andreev@redpencil.io>
2020-11-12 17:23:33 +0100

    fixed some bugs

--- app/components/editor-status-pill.js
+++ app/components/editor-status-pill.js
@@ -13,22 +13,3 @@ export default Component.extend({
     return "action";
   })
 });
-
-// import { computed } from '@ember/object';
-// import Component from '@ember/component';
-
-// export default Component.extend({
-//   tagName: '',
-//   editorStatusClass: computed('ontwerp-besluit-status.id', function() {
-//     const id = this.get('ontwerp-besluit-status.id');
-//     switch(id) {
-//       case "a1974d071e6a47b69b85313ebdcef9f7":
-//         return "concept";
-//       case "7186547b61414095aa2a4affefdcca67":
-//         return "gepubliceerd"
-//       case "ef8e4e331c31430bbdefcdb2bdfbcc06":
-//         return "geagendeerd";
-//     }
-//     return "action";
-//   })
-// });
