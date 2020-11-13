4cbfc94ea0ce19a9cd238df827de591b69ad6dc0
Sergey Andreev <sergey.andreev@redpencil.io>
2020-11-13 11:02:12 +0100

    added back-links to draft decisions

---
+++ app/components/zitting-link.js
@@ -0,0 +1,4 @@
+import Component from '@glimmer/component';
+
+export default class ZittingLinkComponent extends Component {
+}
--- app/controllers/inbox/draft-decisions.js
+++ app/controllers/inbox/draft-decisions.js
@@ -1,15 +1,31 @@
-import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
-import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import {action} from '@ember/object';
+import agenda from '../documents/show/publish/agenda';
 
-export default Controller.extend(DefaultQueryParamsMixin, {
-  currentSession: service(),
-  sort: '-current-version.updated-on',
+export default class InboxDraftDecisionsController extends Controller.extend(DefaultQueryParamsMixin) {
+  @service currentSession;
+  @service store;
+  sort='-current-version.updated-on';
 
-  actions: {
-    openNewDocument() {
-      this.transitionToRoute('draft-decisions.new');
-    }
+  @action
+  openNewDocument() {
+    this.transitionToRoute('draft-decisions.new');
   }
-});
+
+  @action
+  async openZitting(documentContainer){
+
+    let behandeling=await this.store.query("behandeling-van-agendapunt", {"filter[document-container][:id:]": documentContainer.id });
+    behandeling=behandeling.firstObject;
+
+    let agendapunt=await this.store.query("agendapunt", {"filter[behandeling][:id:]": behandeling.id });
+    agendapunt=agendapunt.firstObject;
+
+    let zitting=await this.store.query("zitting", {"filter[agendapunten][:id:]": agendapunt.id });
+    zitting=zitting.firstObject;
+
+    this.transitionToRoute('meetings.edit', zitting.id);
+  }
+}
