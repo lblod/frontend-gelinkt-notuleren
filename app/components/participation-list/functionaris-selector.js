81cda4e607cfeaa2d4facb364b42a669ff015947
abeforgit <arnebertrand@gmail.com>
2020-12-04 18:37:44 +0100

    Fix functionaris-selector

--- app/components/participation-list/functionaris-selector.js
+++ app/components/participation-list/functionaris-selector.js
@@ -2,8 +2,13 @@ import Component from '@glimmer/component';
 import { action } from "@ember/object";
 import { timeout } from 'ember-concurrency';
 import {task} from 'ember-concurrency-decorators';
+import { inject as service } from '@ember/service';
+
+
 
 export default class ParticipationListFunctionarisSelectorComponent extends Component {
+
+  @service store;
   @action
   select(value) {
     this.args.onSelect(value);
@@ -13,7 +18,7 @@ export default class ParticipationListFunctionarisSelectorComponent extends Comp
     yield timeout(300);
     let queryParams = {
       sort: 'is-bestuurlijke-alias-van.achternaam',
-      'filter[bekleedt][bevat-in][:uri:]': this.args.bestuursorgaan.uri,
+      'filter[bekleedt][bevat-in][:uri:]': this.args.bestuursorgaan.get("uri"),
       'filter[is-bestuurlijke-alias-van][achternaam]': searchData,
       page: { size: 100 }
     };
