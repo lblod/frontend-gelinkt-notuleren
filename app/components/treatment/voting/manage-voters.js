78ce38ce91692cf8ed43f65cc62e8049f1fd711c
abeforgit <arnebertrand@gmail.com>
2020-12-05 20:07:20 +0100

    Improve spacing

--- app/components/treatment/voting/manage-voters.js
+++ app/components/treatment/voting/manage-voters.js
@@ -1,7 +1,4 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { task } from 'ember-concurrency-decorators';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 /** @typedef {import("../../../models/stemming").default} Stemming */
 /** @typedef {import("../../../models/mandataris").default} Mandataris */
