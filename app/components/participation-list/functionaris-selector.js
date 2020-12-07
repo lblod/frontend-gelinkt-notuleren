2bdb052b703aabd43f6219e7c1102a880adf9b50
Niels Vandekeybus <progster@gmail.com>
2020-12-07 12:12:38 +0100

    fetch secretaris correctly

--- app/components/participation-list/functionaris-selector.js
+++ app/components/participation-list/functionaris-selector.js
@@ -3,25 +3,38 @@ import { action } from "@ember/object";
 import { timeout } from 'ember-concurrency';
 import {task} from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
-
+import { tracked } from '@glimmer/tracking';
 
 
 export default class ParticipationListFunctionarisSelectorComponent extends Component {
+  @tracked options = [];
+
+  constructor() {
+    super(...arguments);
+    this.loadData.perform();
+  }
 
   @service store;
+  @service currentSession;
   @action
   select(value) {
     this.args.onSelect(value);
   }
   @task
-  *searchByName(searchData){
-    yield timeout(300);
+  *loadData(){
+    const group = yield this.currentSession.group;
+    const bestuursorganen = yield this.store.query('bestuursorgaan', {
+      filter: {
+        "is-tijdsspecialisatie-van": {
+          "bestuurseenheid": { ":id:": group.id },
+          "classificatie": {":id:": "39854196-f214-4688-87a1-d6ad12baa2fa,11f0af9e-016c-4e0b-983a-d8bc73804abc"}
+        }
+      }
+    });
     let queryParams = {
       sort: 'is-bestuurlijke-alias-van.achternaam',
-      'filter[bekleedt][bevat-in][:uri:]': this.args.bestuursorgaan.get("uri"),
-      'filter[is-bestuurlijke-alias-van][achternaam]': searchData,
-      page: { size: 100 }
+      'filter[bekleedt][bevat-in][:id:]': bestuursorganen.map((b) => b.id).join(',')
     };
-    return yield this.store.query('functionaris', queryParams);
+    this.options = yield this.store.query('functionaris', queryParams);
   }
 }
