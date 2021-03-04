fb8ecbf7570e1e9b278b43c0e22f6e913eb2f960
Niels Vandekeybus <progster@gmail.com>
2021-03-04 22:26:12 +0100

    fix import of JSONAPISerializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,7 +1,7 @@
-import DS from 'ember-data';
+import JSONAPISerializer from '@ember-data/serializer/json-api';
 import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 
-export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
+export default JSONAPISerializer.extend(DataTableSerializerMixin, {
     serializeAttribute(snapshot, json, key, attributes) {
     if (key !== 'uri')
       this._super(snapshot, json, key, attributes);
