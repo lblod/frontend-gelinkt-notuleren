461f2d0c6775961a3f51aa6a76a19f87a80e14ea
Niels Vandekeybus <progster@gmail.com>
2021-03-05 14:22:52 +0100

    modernize editor-document-title component
    refactored the component to a glimmer component, but also changed how it works
    slightly:
    
    - removed integration with the document-title plugin, as that plugin is no
    longer in use
    - if a document doens't have a name yet, immediately start in edit mode
    - provided a button to toggle edit mode as clicking on the h1 was no longer
    possible
    - the button has the added benefit of being more accessible
    
    it was not my goal to change how this component worked, so this should not be
    seen as a solution to other usability issues

--- app/components/editor-document-title.js
+++ app/components/editor-document-title.js
@@ -1,58 +1,38 @@
-import { schedule } from '@ember/runloop';
-import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import { task } from 'ember-concurrency';
+import Component from '@glimmer/component';
+import {action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  active: false,
-  editorDocument: null,
-  titlePlugin: service('rdfa-editor-document-title-plugin'),
-  overruledTitle: '',
-  generatedTitle: '',
+export default class EditorDocumentTitleComponent extends Component {
+  @tracked _active = false;
+  @tracked _title;
 
-  titleObserver: task(function *(){  // eslint-disable-line require-yield
-    if (this.titlePlugin)
-      this.set('generatedTitle', this.titlePlugin.title);
-    if (this.overruledTitle && this.overruledTitle.length > 0 ) {
-      this.set('editorDocument.title', this.overruledTitle);
+  get active() {
+    return this._active || (!this._title && ! this.args.title);
+  }
+
+  get title() {
+    if (this._title) {
+      return this._title;
     }
-    else if (this.titlePlugin && this.titlePlugin.title.length > 0) {
-      this.set('editorDocument.title', this.titlePlugin.title);
+    else if (this.args.title) {
+      return this.args.title;
     }
     else {
-      this.set('editorDocument.title', 'Naamloos document');
-    }
-  }).keepLatest(),
-
-  didReceiveAttrs(){
-    this._super(...arguments);
-    if(this.editorDocument.title) {
-      this.set('overruledTitle', this.editorDocument.title);
-      this.set('generatedTitle', this.editorDocument.title);
-    }
-
-    if(this.titlePlugin)
-      this.titlePlugin.addObserver('title', () => { return this.titleObserver.perform(); });
-  },
-
-  actions:{
-    updateTitle(){
-    },
-    toggleActive(){
-      this.set('active', !this.active);
-      if (this.active)
-        schedule('afterRender', () => this.element.querySelector('input').focus());
-      else {
-        this.set('overruledTitle', this.editorDocument.title);
-        const overruledTitle = this.overruledTitle.slice().trim();
-        if(overruledTitle.length === 0) {
-          if(this.generatedTitle.length > 0) {
-            this.set('editorDocument.title', this.generatedTitle);
-          } else {
-            this.set('editorDocument.title', 'Naamloos document');
-          }
-        }
-      }
+      return "Naamloos document";
     }
   }
-});
+
+  @action
+  setTitle(event) {
+    this._title = event.target.value;
+    if (this.args.onChange) {
+      this.args.onChange(this._title);
+    }
+  }
+
+  @action
+  toggleActive() {
+    this._active = ! this._active;
+    console.log(this._active);
+  }
+}
