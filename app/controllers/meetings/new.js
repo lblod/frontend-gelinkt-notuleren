76a4c73afcb3e99e2efe17447fdddcf4d2e0e7e6
Niels Vandekeybus <progster@gmail.com>
2021-03-12 10:21:12 +0100

    remove unused var

--- app/controllers/meetings/new.js
+++ app/controllers/meetings/new.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { action } from "@ember/object";
-import { inject as service } from '@ember/service';
 
 export default class MeetingsNewController extends Controller {
   @action
