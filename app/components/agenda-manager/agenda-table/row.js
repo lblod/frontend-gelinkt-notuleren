374eda57a64a78dbc9e9f2688b52b94e39d1f377
Sergey Andreev <sergey.andreev@redpencil.io>
2021-03-23 07:00:07 +0100

    disabled editing of behandeling

--- app/components/agenda-manager/agenda-table/row.js
+++ app/components/agenda-manager/agenda-table/row.js
@@ -1,8 +1,8 @@
 import Component from '@glimmer/component';
-import { DRAFT_STATUS_ID, PUBLISHED_STATUS_ID, PLANNED_STATUS_ID } from 'frontend-gelinkt-notuleren/utils/constants';
+import { PUBLISHED_STATUS_ID } from 'frontend-gelinkt-notuleren/utils/constants';
 import { tracked } from "@glimmer/tracking";
 import { task } from "ember-concurrency-decorators";
-import { get, action } from "@ember/object";
+import { get } from "@ember/object";
 import { inject as service } from '@ember/service';
 
 export default class AgendaManagerAgendaTableRowComponent extends Component {
@@ -23,25 +23,9 @@ export default class AgendaManagerAgendaTableRowComponent extends Component {
     })).firstObject;
 
     const statusId=get(behandeling, "documentContainer.status.id");
-    const editorStatus=this.editorStatus(statusId);
 
-    if(editorStatus=="published"){
+    if(statusId==PUBLISHED_STATUS_ID){
       this.published=true;
-    }
-  }
-
-  editorStatus(statusId) {
-    if (statusId == DRAFT_STATUS_ID) {
-      return "draft";
-    }
-    else if (statusId == PLANNED_STATUS_ID) {
-      return "planned";
-    }
-    else if (statusId == PUBLISHED_STATUS_ID) {
-      return "published";
-    }
-    else {
-      return "unknown";
     }
   }
 }
