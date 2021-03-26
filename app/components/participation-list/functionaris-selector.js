b217dbe74cfde590686b758da3ba0368cc8c5ec4
Niels Vandekeybus <progster@gmail.com>
2021-03-26 14:26:25 +0100

    secretary for provinces should be from different type of admin body

--- app/components/participation-list/functionaris-selector.js
+++ app/components/participation-list/functionaris-selector.js
@@ -3,7 +3,10 @@ import {task} from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
-
+const PROVINCE_CLASSIFICATION_ID = '5ab0e9b8a3b2ca7c5e000000';
+const GRIFFIER_CLASSIFICATION_ID = '5ab19107-82d2-4273-a986-3da86fda050d';
+const ALGEMEEN_DIRECTEUR_CLASSIFICATION_ID = '39854196-f214-4688-87a1-d6ad12baa2fa';
+const ADJUNCT_ALGEMEEN_DIRECTEUR_CLASSIFICATION_ID = '11f0af9e-016c-4e0b-983a-d8bc73804abc';
 export default class ParticipationListFunctionarisSelectorComponent extends Component {
   @tracked options = [];
 
@@ -18,17 +21,22 @@ export default class ParticipationListFunctionarisSelectorComponent extends Comp
   @task
   *loadData(){
     const group = yield this.currentSession.group;
+    const adminUnitClassification = yield group.classificatie;
+    let relevantBestuursOrgaanClassifications = `${ALGEMEEN_DIRECTEUR_CLASSIFICATION_ID},${ADJUNCT_ALGEMEEN_DIRECTEUR_CLASSIFICATION_ID}`;
+    if (adminUnitClassification.id === PROVINCE_CLASSIFICATION_ID) {
+      relevantBestuursOrgaanClassifications = GRIFFIER_CLASSIFICATION_ID;
+    }
     const bestuursorganen = yield this.store.query('bestuursorgaan', {
       filter: {
         "is-tijdsspecialisatie-van": {
           "bestuurseenheid": { ":id:": group.id },
-          "classificatie": {":id:": "39854196-f214-4688-87a1-d6ad12baa2fa,11f0af9e-016c-4e0b-983a-d8bc73804abc"}
+          "classificatie": {":id:": relevantBestuursOrgaanClassifications }
         }
       }
     });
     let queryParams = {
       sort: 'is-bestuurlijke-alias-van.achternaam',
-      'filter[bekleedt][bevat-in][:id:]': bestuursorganen.map((b) => b.id).join(',')
+      'filter[bekleedt][bevat-in][:id:]': bestuursorganen.map((b) => b.id).join(','),
     };
     this.options = yield this.store.query('functionaris', queryParams);
   }
