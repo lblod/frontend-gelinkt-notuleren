2e8d1c014aaec9778673044584f4ab123bd18b6e
Niels Vandekeybus <progster@gmail.com>
2021-03-26 10:51:55 +0100

    make voter table a bit more robust wrt faulty data

--- app/components/treatment/voting/voter-table.js
+++ app/components/treatment/voting/voter-table.js
@@ -14,12 +14,12 @@ export default class TreatmentVotingVoterTableComponent extends Component {
   /** @type {EditStemmingService} */
   @service editStemming;
   get voters() {
-    const map = this.editStemming.votingMap
+    const map = this.editStemming.votingMap;
     const entries = [...map];
-    return entries.sort((entry1, entry2) =>
-      entry1[0].isBestuurlijkeAliasVan
-        .get("achternaam")
-        .localeCompare(entry2[0].isBestuurlijkeAliasVan.get("achternaam"))
-    );
+    return entries.sort((entry1, entry2) => {
+      const lastName1 = entry1[0].get("isBestuurlijkeAliasVan.achternaam") || "";
+      const lastName2 = entry2[0]?.get("isBestuurlijkeAliasVan.achternaam") || "";
+      return lastName1.localeCompare(lastName2);
+    });
   }
 }
