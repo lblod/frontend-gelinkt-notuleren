5e9380467432bcc6f99bcfd2767a1e9375493079
Oscar <lagartoverde97@gmail.com>
2021-04-13 12:27:26 +0200

    Fixed bug caching issue after saving on existing agendpoint GN-2519

--- app/routes/agendapoints/edit.js
+++ app/routes/agendapoints/edit.js
@@ -1,23 +1,26 @@
 import RSVP from 'rsvp';
 import Route from '@ember/routing/route';
+import { action } from "@ember/object";
 
-export default Route.extend({
+export default class AgendapointsEditRoute extends Route {
   async model(params){
     const container = await this.store.findRecord('document-container', params.id, { include: 'status' });
     return RSVP.hash({
       documentContainer: container,
       editorDocument: await container.get('currentVersion')
     });
-  },
-
-  actions: {
-    error(error /*, transition */) {
-      if (error.errors && error.errors[0].status === "404") {
-        this.transitionTo('route-not-found');
-      } else {
-        // Let the route above this handle the error.
-        return true;
-      }
+  }
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    controller._editorDocument = null;
+  }
+  @action
+  error(error /*, transition */) {
+    if (error.errors && error.errors[0].status === "404") {
+      this.transitionTo('route-not-found');
+    } else {
+      // Let the route above this handle the error.
+      return true;
     }
   }
-});
+}
