5061f38090ba8161352ed59a8aa77d020fb98fc8
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-04-15 14:33:39 +0200

    Move the custom-inflector-rules.js file
    The model folder should only include actual Models.

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'frontend-gelinkt-notuleren/config/environment';
-import './models/custom-inflector-rules';
+import './config/custom-inflector-rules';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
