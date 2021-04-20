014c17ee9c2488f11959fae9a29d78fdb1ce8d12
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-04-20 18:19:26 +0200

    Install `ember-unique-id-helper-polyfill`
    This polyfill implements RFC659 so it's safe to use until the actual implementation is released in ember-source.

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane'
+  extends: 'octane',
+  rules: {
+    'no-curly-component-invocation': {
+      allow: ['unique-id'],
+    },
+    'no-implicit-this': {
+      allow: ['unique-id'],
+    }
+  }
 };
