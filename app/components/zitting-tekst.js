5f7fa6de4f01ae0df3e67f9b725a65bcebc068dd
Sergey Andreev <sergey.andreev@redpencil.io>
2021-05-06 11:45:43 +0200

    fixed linting errors

--- app/components/zitting-tekst.js
+++ app/components/zitting-tekst.js
@@ -1,8 +1,6 @@
 import Component from '@glimmer/component';
-import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 import { task } from "ember-concurrency-decorators";
-import { inject as service } from "@ember/service";
 
 export default class ZittingTekstComponent extends Component {
   constructor(...args){
