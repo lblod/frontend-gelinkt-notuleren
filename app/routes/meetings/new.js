7f548b144165a0b6f62dacce1756378ad7c4c344
Niels Vandekeybus <progster@gmail.com>
2021-05-07 10:03:34 +0200

    protect meetings new route

--- app/routes/meetings/new.js
+++ app/routes/meetings/new.js
@@ -1,6 +1,14 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class MeetingsNewRoute extends Route {
+  @service currentSession;
+  beforeModel() {
+    if(!this.currentSession.canWrite) {
+      this.transitionTo('inbox.meetings');
+    }
+  }
+
   model(){
     const now = new Date();
     return this.store.createRecord('zitting', {
