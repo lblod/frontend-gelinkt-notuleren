e8e49b37a2cd701c797f98266be4d231ba2afd3c
Niels Vandekeybus <progster@gmail.com>
2021-05-07 10:00:15 +0200

    protect agendapoint new route

--- app/routes/agendapoints/new.js
+++ app/routes/agendapoints/new.js
@@ -1,6 +1,16 @@
+import RSVP from 'rsvp';
 import Route from '@ember/routing/route';
+import { action } from "@ember/object";
+import { inject as service } from '@ember/service';
 
 export default class AgendapointsNewRoute extends Route {
+  @service currentSession;
+  beforeModel() {
+    if(!this.currentSession.canWrite) {
+      this.transitionTo('inbox.agendapoints');
+    }
+  }
+
   model() {
     return this.store.createRecord('editor-document');
   }
