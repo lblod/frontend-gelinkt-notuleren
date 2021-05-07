d1ac26a8e2e29381c4eaec9ee13bded2378a56b7
Niels Vandekeybus <progster@gmail.com>
2021-05-07 10:02:38 +0200

    move route protection to beforeModel hook

--- app/routes/agendapoints/edit.js
+++ app/routes/agendapoints/edit.js
@@ -4,12 +4,17 @@ import { action } from "@ember/object";
 import { inject as service } from '@ember/service';
 
 export default class AgendapointsEditRoute extends Route {
-  @service currentSession;  
-  async model(params){
-    if(!this.currentSession.canWrite && this.currentSession.canRead) {
-      this.transitionTo('agendapoints.show', params.id);
+  @service currentSession;
+
+  beforeModel(transition) {
+    if(!this.currentSession.canWrite) {
+      const id = transition.to.params?.id;
+      this.transitionTo('agendapoints.show', id);
       return;
     }
+  }
+
+  async model(params){
     const container = await this.store.findRecord('document-container', params.id, { include: 'status' });
     return RSVP.hash({
       documentContainer: container,
@@ -20,6 +25,7 @@ export default class AgendapointsEditRoute extends Route {
     super.setupController(controller, model);
     controller._editorDocument = null;
   }
+
   @action
   error(error /*, transition */) {
     if (error.errors && error.errors[0].status === "404") {
