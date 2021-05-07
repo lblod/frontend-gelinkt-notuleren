761dddf5d27275bf81a2b45da59853526229ab6a
Sergey Andreev <sergey.andreev@redpencil.io>
2021-05-07 08:20:23 +0200

    changed according to feature passpor

---
+++ app/components/zitting-text-document-container.js
@@ -0,0 +1,56 @@
+import Component from '@glimmer/component';
+import { tracked } from "@glimmer/tracking";
+import { task } from "ember-concurrency-decorators";
+import { action } from "@ember/object";
+
+export default class ZittingTextDocumentContainerComponent extends Component {
+  constructor(...args) {
+    super(...args);
+    this.getText.perform();
+  }
+
+  profile = 'none';
+  editor;
+  type = this.args.type;
+  text;
+
+  editorOptions = {
+    showToggleRdfaAnnotations: false,
+    showInsertButton: true,
+    showRdfa: false,
+    showRdfaHighlight: false,
+    showRdfaHover: false
+  };
+
+  @task
+  *getText() {
+    const zitting = yield this.args.zitting;
+    if (this.type === 'ext:intro') {
+      this.text = yield zitting.intro;
+    }
+    else if (this.type === 'ext:outro') {
+      this.text = yield zitting.outro;
+    }
+  }
+
+  @task
+  *saveText() {
+    const zitting = yield this.args.zitting;
+    this.text = this.editor.htmlContent;
+
+    if (this.type === 'ext:intro') {
+      zitting.intro = this.text;
+    }
+    else if (this.type === 'ext:outro') {
+      zitting.outro = this.text;
+    }
+    yield zitting.save();
+  }
+
+  @action
+  handleRdfaEditorInit(editor) {
+    editor.setHtmlContent(this.text);
+    this.editor = editor;
+  }
+
+}
