de1e5da7c9dc1d5e4caa60d6fd29ced97d1e3568
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-05-28 15:34:52 +0200

    Meeting creation flow changes
    This simplifies the meeting creation flow. It now simply shows a modal in which the user can enter the needed data before being redirected to the edit page.

---
+++ app/controllers/inbox/meetings/new.js
@@ -0,0 +1,43 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { dropTask } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+
+export default class InboxMeetingsNewController extends Controller {
+  @service router;
+
+  get meeting() {
+    return this.model;
+  }
+
+  @dropTask
+  * saveMeetingTask(event) {
+    event.preventDefault();
+
+    let bestuursorgaan = yield this.meeting.bestuursorgaan;
+    if (!bestuursorgaan) {
+      this.meeting.errors.add('bestuursorgaan', 'inbox.meetings.new.meeting.errors.administrativeBody.required');
+    }
+
+    if (this.meeting.isValid) {
+      yield this.meeting.save();
+      this.router.replaceWith('meetings.edit', this.meeting.id);
+    }
+  }
+
+  @action
+  updateAdministrativeBody(administrativeBody) {
+    this.meeting.bestuursorgaan = administrativeBody;
+    this.meeting.errors.remove('bestuursorgaan');
+  }
+
+  @action
+  cancelMeetingCreation() {
+    this.meeting.destroyRecord();
+
+    // Ember has a bug where using the router service here, reruns the parent's model hooks.
+    // More info: https://github.com/emberjs/ember.js/issues/19497
+    // TODO use the router service once the bug is fixed
+    this.replaceRoute('inbox.meetings');
+  }
+}
