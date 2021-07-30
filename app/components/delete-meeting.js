6554a0ee8143ef88d2eb1a131618bb782541c4e2
Sergey Andreev <sergey.andreev@redpencil.io>
2021-07-30 12:11:23 +0200

    added basic functionality

---
+++ app/components/delete-meeting.js
@@ -0,0 +1,16 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class DeleteMeetingComponent extends Component {
+  @service router;
+  @tracked displayDeleteModal=false;
+  @task
+  *deleteMeeting(){
+    yield this.args.meeting.destroyRecord();
+    this.displayDeleteModal=false;
+    this.router.transitionTo('inbox.meetings');
+  }
+}
