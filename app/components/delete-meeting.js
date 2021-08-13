8e064fd5e070dfecd64f85fd013788f2c1797bfe
Sergey Andreev <sergey.andreev@redpencil.io>
2021-08-13 12:44:47 +0200

    removed unused import

--- app/components/delete-meeting.js
+++ app/components/delete-meeting.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class DeleteMeetingComponent extends Component {
