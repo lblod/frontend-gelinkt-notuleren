e3c3a56a3dc930b112b580a0580a600c4fb9b204
Niels Vandekeybus <progster@gmail.com>
2021-08-24 14:18:27 +0200

    remove log statement

--- app/routes/meetings/publish/uittreksels/show.js
+++ app/routes/meetings/publish/uittreksels/show.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class MeetingsPublishUittrekselsShowRoute extends Route {
   async model(params) {
-    console.log(params);
     return this.store.findRecord('behandeling-van-agendapunt', params.treatment_id, { include: 'onderwerp.zitting'});
   }
 
