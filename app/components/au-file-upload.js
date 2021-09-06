503ec244686f3a8ea64d993bd91b93ca91aad443
Niels Vandekeybus <progster@gmail.com>
2021-09-06 15:54:35 +0200

    use au-file-upload from appuniversum
    depends on https://github.com/appuniversum/ember-appuniversum/pull/139 , so
    needs a version bump of appuniversum before it can be merged

--- app/components/au-file-upload.js
+++
@@ -1,5 +0,0 @@
-import VoMuFileDropzoneComponent from '@lblod/ember-vo-mu-file-upload/components/vo-mu-file-dropzone';
-
-export default class AuFileUpload extends VoMuFileDropzoneComponent {
-  
-}
