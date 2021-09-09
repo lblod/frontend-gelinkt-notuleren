84b131cace1c8072937e96f30259c4821b88e016
Oscar <lagartoverde97@gmail.com>
2021-09-09 10:06:00 +0200

    improved component and fixed all bugs

--- app/components/signatures/publication-status.js
+++ app/components/signatures/publication-status.js
@@ -1,14 +1,25 @@
 import Component from '@glimmer/component';
+import {tracked} from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 
 export default class SignaturesPublicationStatus extends Component {
+  @tracked signedResources = [];
+  @tracked publishedResource
+  @tracked ready = false;
+  @action  
+  async loadResource() {
+    console.log(this.args.versionedResource.get('signedResources'));
+    this.signedResources = await this.args.versionedResource.get('signedResources');
+    this.publishedResource = await this.args.versionedResource.get('publishedResource');
+    this.ready = true;
+  }
   get publicationStatus(){
-    const versionedResource = this.args.versionedResource;
-    if (versionedResource.get('publishedResource')) {
+    if (this.publishedResource) {
       return {label: 'Gepubliceerd', color: 'action'};
-    }else if (versionedResource.get('signedResources').length === 1) {
+    }else if (this.signedResources.length === 1) {
       return {label: 'Eerste ondertekening verkregen', color: 'warning'};
-    } else if (versionedResource.get('signedResources').length === 2) {
+    } else if (this.signedResources.length === 2) {
       return {label: 'Getekend', color: 'success'};
     } else {
       return {label: 'Concept'};
