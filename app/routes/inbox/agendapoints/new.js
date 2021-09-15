fe50effa03ff7aaad568d0ca9bb57a0d50668783
Niels Vandekeybus <progster@gmail.com>
2021-09-15 16:31:25 +0200

    redirect if person cant create new agendapoints

--- app/routes/inbox/agendapoints/new.js
+++ app/routes/inbox/agendapoints/new.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class InboxAgendapointsNewRoute extends Route {
+  @service currentSession;
+  @service router;
+
+  beforeModel() {
+    if (!this.currentSession.canWrite) {
+      this.router.replaceWith('inbox.agendapoints');
+    }
+  }
 }
