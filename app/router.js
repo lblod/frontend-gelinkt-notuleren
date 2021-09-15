154c88c124bfceb2235b3a1b81bee475c3079d16
Niels Vandekeybus <progster@gmail.com>
2021-09-15 09:29:19 +0200

    add route

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  this.route('switch-login');
   this.route('inbox', function() {
     this.route('trash');
     this.route('agendapoints');
