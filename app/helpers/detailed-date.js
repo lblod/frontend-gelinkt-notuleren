0c6e83f8efa7128e4bf1b39c43049fab892164cf
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-01-14 16:09:53 +0100

    Changed some formatting to Intl
    Changed some date formatting to Intl instead of using date-fns. Not yet
    possible everywhere, due to complex relative formatting not being
    readily available in Intl.

--- app/helpers/detailed-date.js
+++ app/helpers/detailed-date.js
@@ -1,12 +1,11 @@
 import { helper } from '@ember/component/helper';
-import lightFormat from 'date-fns/lightFormat';
 
 export default helper(function detailedDate([datetime]/*, hash*/) {
   //If not a date (e.g. date is undefined) return "" for printing on screen.
   if (!(datetime instanceof Date)) return "";
 
   try {
-    return lightFormat(datetime, 'dd/MM/yyyy HH:mm');
+    return Intl.DateTimeFormat("nl-BE", { dateStyle: "short", timeStyle: "short" }).format(datetime);
   }
   catch(e) {
     console.error(e);
--- app/helpers/plain-date.js
+++ app/helpers/plain-date.js
@@ -1,12 +1,11 @@
 import { helper } from '@ember/component/helper';
-import lightFormat from 'date-fns/lightFormat';
 
 export default helper(function plainDate([datetime]/*, hash*/) {
   //If not a date (e.g. date is undefined) return "" for printing on screen.
   if (!(datetime instanceof Date)) return "";
 
   try {
-    return lightFormat(datetime, 'dd/MM/yyyy');
+    return Intl.DateTimeFormat("nl-BE", { dateStyle: "short" }).format(datetime);
   }
   catch(e) {
     console.error(e);
