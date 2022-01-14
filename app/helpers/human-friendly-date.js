d2db103d38c4685a9aaa67599969e3dc2c1b858c
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-01-14 14:17:43 +0100

    Added date object type check
    With an instance check for Date, the helper no longer tries to format
    things that are not dates, such as undefined or other objects.
    In some instances, a date is left undefined, such as the end date on
    meetings if the meeting isn't finished yet. We don't want unnecessary
    errors being printend on the console because of this.

--- app/helpers/human-friendly-date.js
+++ app/helpers/human-friendly-date.js
@@ -4,6 +4,9 @@ import { nl } from 'date-fns/locale';
 
 // this is a helper to mimic the moment-calendar behaviour
 export default helper(function humanFriendlyDate([referenceDatetime]/*, hash*/) {
+  //If not a date (e.g. date is undefined) return "" for printing on screen.
+  if (!(datetime instanceof Date)) return "";
+
   try {
     return formatRelative(referenceDatetime, new Date(), { locale: nl });
   }
--- app/helpers/plain-date.js
+++ app/helpers/plain-date.js
@@ -2,10 +2,13 @@ import { helper } from '@ember/component/helper';
 import lightFormat from 'date-fns/lightFormat';
 
 export default helper(function plainDate([datetime]/*, hash*/) {
+  //If not a date (e.g. date is undefined) return "" for printing on screen.
+  if (!(datetime instanceof Date)) return "";
+
   try {
     return lightFormat(datetime, 'dd/MM/yyyy');
   }
-  catch(e){
+  catch(e) {
     console.error(e);
     return "";
   }
