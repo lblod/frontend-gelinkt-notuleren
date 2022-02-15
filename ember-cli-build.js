6211bfc19d31568b42cd055c47efb751b736656c
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-02-15 18:39:44 +0100

    Removed all references to FlatPickr
    FlatPickr was used as a date/time picker for some plugins, but is no
    longer used.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -31,10 +31,6 @@ module.exports = function (defaults) {
       cascade: true,
       sourcemap: !envIsProduction
     },
-    flatpickr: {
-      locales: ['nl'],
-      theme: 'light'
-    },
     sourcemaps: {
       enabled: !envIsProduction,
       extensions: ['js', 'css']
