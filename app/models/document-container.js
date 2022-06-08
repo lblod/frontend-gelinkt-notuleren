f597f9f7a6c4740177e2c292a12437c7777b3fb8
Niels Vandekeybus <progster@gmail.com>
2022-06-08 15:44:20 +0200

    add type and identifier to overview of irg archive

--- app/models/document-container.js
+++ app/models/document-container.js
@@ -1,11 +1,14 @@
-import Model, { belongsTo, hasMany } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class DocumentContainerModel extends Model {
+  @attr identifier;
   @hasMany('editor-document', { inverse: 'documentContainer' }) revisions;
   @belongsTo('editor-document', { inverse: null }) currentVersion;
   @belongsTo('concept', { inverse: null }) status;
   @belongsTo('editor-document-folder', { inverse: null }) folder;
   @belongsTo('bestuurseenheid', { inverse: null }) publisher;
+  @belongsTo('concept', { inverse: null }) type;
+
   @hasMany('versioned-agenda') versionedAgendas;
   @hasMany('versioned-notulen') versionedNotulen;
   @hasMany('versioned-besluiten-lijst') versionedBesluitenLijsten;
