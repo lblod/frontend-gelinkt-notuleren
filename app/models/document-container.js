a1f22010eb68b1aaf0ef5a7f9aa68b43cd5b7544
Sergey Andreev <sergey.andreev@redpencil.io>
2022-06-09 17:43:30 +0200

    fixed linting

--- app/models/document-container.js
+++ app/models/document-container.js
@@ -1,4 +1,4 @@
-import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
+import Model, { belongsTo, hasMany } from '@ember-data/model';
 
 export default class DocumentContainerModel extends Model {
   @hasMany('editor-document', { inverse: 'documentContainer' }) revisions;
