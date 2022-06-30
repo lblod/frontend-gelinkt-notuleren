cfb587ce42746e458a4a6b80da2702d12b8ad262
Sergey Andreev <sergey.andreev@redpencil.io>
2022-06-30 16:23:17 +0200

    initial commit

--- app/controllers/inbox/agendapoints.js
+++ app/controllers/inbox/agendapoints.js
@@ -1,19 +1,43 @@
 import Controller from '@ember/controller';
-import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { task } from 'ember-concurrency';
 
-export default class InboxDraftDecisionsController extends Controller.extend(
-  DefaultQueryParamsMixin
-) {
+export default class InboxDraftDecisionsController extends Controller {
+  @tracked page = 0;
+  @tracked size = 10;
+  @tracked filter = '';
+  @tracked searchValue = this.filter;
+  @tracked debounceTime = 2000;
+  @tracked debouncing = false;
+
   @service currentSession;
   @service router;
   sort = '-current-version.updated-on';
+
+  @task
+  *updateFilter(event) {
+    const input = event.target.value;
+    this.searchValue = input;
+    if (!this.debouncing) {
+      this.isDebouncing = true;
+      yield this.timeout(this.debounceTime);
+      this.filter = this.searchValue;
+      this.page = 0;
+      this.debouncing = false;
+    }
+  }
+
+  timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 
   @action
   openNewDocument() {
     this.router.transitionTo('agendapoints.new');
   }
+
   get readOnly() {
     return !this.currentSession.canWrite && this.currentSession.canRead;
   }
--- app/controllers/inbox/irg-archive.js
+++ app/controllers/inbox/irg-archive.js
@@ -1,8 +1,29 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
 
 export default class InboxIrgArchiveController extends Controller {
   @tracked page = 0;
   @tracked size = 10;
   @tracked filter = '';
+  @tracked searchValue = this.filter;
+  @tracked debounceTime = 2000;
+  @tracked debouncing = false;
+
+  @task
+  *updateFilter(event) {
+    const input = event.target.value;
+    this.searchValue = input;
+    if (!this.debouncing) {
+      this.isDebouncing = true;
+      yield this.timeout(this.debounceTime);
+      this.filter = this.searchValue;
+      this.page = 0;
+      this.debouncing = false;
+    }
+  }
+
+  timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
