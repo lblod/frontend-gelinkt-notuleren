726e3d9503cb4731ef9086c4265b8426487c9f60
Oscar <lagartoverde97@gmail.com>
2022-09-19 13:38:09 +0200

    add save

--- app/controllers/regulatory-statements/edit.js
+++ app/controllers/regulatory-statements/edit.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { PLUGIN_CONFIGS } from 'frontend-gelinkt-notuleren/config/constants';
+import { task } from 'ember-concurrency';
 
 export default class RegulatoryStatementsRoute extends Controller {
+  editor;
   plugins = [
     'article-structure',
     { name: 'rdfa-toc', options: { config: PLUGIN_CONFIGS.TABLE_OF_CONTENTS } },
@@ -20,8 +22,32 @@ export default class RegulatoryStatementsRoute extends Controller {
     return this.model.documentContainer;
   }
 
+  @task
+  *saveTask() {
+    console.log('saving')
+    if (!this.editorDocument.title) {
+      this.hasDocumentValidationErrors = true;
+    } else {
+      this.hasDocumentValidationErrors = false;
+      const html = this.editor.htmlContent;
+      const editorDocument = this.store.createRecord('editor-document');
+      editorDocument.content = html;
+      editorDocument.createdOn = new Date();
+      editorDocument.updatedOn = new Date();
+      editorDocument.title = this.editorDocument.title;
+      editorDocument.previousVersion = this.editorDocument;
+      yield editorDocument.save();
+      this._editorDocument = editorDocument;
+
+      const documentContainer = this.documentContainer;
+      documentContainer.currentVersion = editorDocument;
+      yield documentContainer.save();
+    }
+  }
+
   @action
   handleRdfaEditorInit(controller) {
     controller.setHtmlContent(this.editorDocument.content);
+    this.editor = controller;
   }
 }
