95755fdb080003d25e600f7fdbb44d34d9f43312
sbx6 <shpetim.hoxha@ourcart.com>
2022-10-21 14:34:40 +0200

    fix trash and uittreksels pages

--- app/controllers/inbox/trash.js
+++ app/controllers/inbox/trash.js
@@ -2,11 +2,15 @@ import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { TRASH_CONCEPT_STATUS } from '../../utils/constants';
+import { tracked } from '@glimmer/tracking';
 
 export default class InboxTrashController extends Controller {
   @service currentSession;
   @service store;
   @service router;
+  @tracked debounceTime = 2000;
+  @tracked page = 0;
+  @tracked pageSize = 20;
 
   @action
   async moveToConcepts(documents) {
--- app/controllers/meetings/publish/uittreksels/index.js
+++ app/controllers/meetings/publish/uittreksels/index.js
@@ -1,5 +1,9 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
 export default class MeetingsPublishUittrekselsController extends Controller {
   sort = 'position';
+  @tracked debounceTime = 2000;
+  @tracked page = 0;
+  @tracked pageSize = 20;
 }
