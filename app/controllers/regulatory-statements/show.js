fddd3da9dadf704e0ad7c289624d6ae9494dbaa0
Niels V <niels.vandekeybus@redpencil.io>
2022-10-24 15:05:13 +0200

    use app chrome correctly in show route

--- app/controllers/regulatory-statements/show.js
+++ app/controllers/regulatory-statements/show.js
@@ -4,25 +4,13 @@ import { task } from 'ember-concurrency';
 import generateExportFromEditorDocument from 'frontend-gelinkt-notuleren/utils/generate-export-from-editor-document';
 import { action } from '@ember/object';
 
-export default class AgendapointsShowController extends Controller {
+export default class RegulatoryAttachmentsShowController extends Controller {
   @service currentSession;
   @service router;
 
   @action
   download() {
     generateExportFromEditorDocument(this.model.editorDocument);
-  }
-
-  //Not sure if we want this feature
-  @task
-  *copyAgendapunt() {
-    const response = yield fetch(
-      `/agendapoint-service/${this.model.documentContainer.id}/copy`,
-      { method: 'POST' }
-    );
-    const json = yield response.json();
-    const agendapuntId = json.uuid;
-    yield this.router.transitionTo('agendapoints.edit', agendapuntId);
   }
 
   get readOnly() {
