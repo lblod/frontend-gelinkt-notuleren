cf41c5d8f83eb4c750784ea35da7b52e574231fe
Elena Poelman <elena.poelman@redpencil.io>
2022-10-26 16:18:12 +0200

    link up cancel and insert buttons

--- app/components/editor-plugins/regulatory-statements/sidebar-insert.js
+++ app/components/editor-plugins/regulatory-statements/sidebar-insert.js
@@ -2,12 +2,17 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class manageIntermissionsComponent extends Component {
+export default class RegulatoryStatementsSidebarInsertComponent extends Component {
   @tracked modalEnabled = false;
 
   @action
   toggleModal() {
     console.log('TOGGLE');
     this.modalEnabled = !this.modalEnabled;
+  }
+
+  @action
+  insertRegulatoryStatement(_statement){
+    this.modalEnabled = false;
   }
 }
