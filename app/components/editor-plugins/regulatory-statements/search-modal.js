41cc50cf00b7628d68f68a5e4aeb1b340f1b1147
Elena Poelman <elena.poelman@redpencil.io>
2022-11-03 16:48:13 +0100

    rework fetching regulatory-statements in plugin and add error messages

--- app/components/editor-plugins/regulatory-statements/search-modal.js
+++ app/components/editor-plugins/regulatory-statements/search-modal.js
@@ -3,21 +3,38 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { query } from 'ember-data-resources';
 import { EDITOR_FOLDERS } from '../../../config/constants';
+import { inject as service } from '@ember/service';
 
 export default class RegulatoryStatementsSearchModalComponent extends Component {
-  @tracked searchQuery;
-  @tracked selectedStatement;
+  @service store;
+
+  @tracked searchValue = '';
+  @tracked _selectedStatement;
+  debounceTime = 2000;
 
   regulatoryStatements = query(this, 'document-container', () => ({
     include: 'current-version',
     'filter[folder][:id:]': EDITOR_FOLDERS.REGULATORY_STATEMENTS,
-    ...(this.searchQuery && {
-      'filter[current-version][title]': this.searchQuery,
+    'filter[:has-no:is-part-of]': 'yes',
+    ...(this.searchValue && {
+      'filter[current-version][title]': this.searchValue,
     }),
   }));
 
+  updateFilter(event) {
+    const input = event.target.value;
+    this.searchValue = input;
+    this._selectedStatement = null;
+  }
+
   @action
   selectStatement(statement) {
-    this.selectedStatement = statement;
+    this._selectedStatement = statement;
+  }
+
+  get selectedStatement() {
+    return (
+      this._selectedStatement ?? this.regulatoryStatements.records?.firstObject
+    );
   }
 }
