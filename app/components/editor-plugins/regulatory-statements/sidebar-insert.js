7f4139beb589497a9a117b941a97a7a52a519d64
Elena Poelman <elena.poelman@redpencil.io>
2022-11-14 10:03:39 +0100

    change way in how we determine whether the 'insert statement' button should be disabled

--- app/components/editor-plugins/regulatory-statements/sidebar-insert.js
+++ app/components/editor-plugins/regulatory-statements/sidebar-insert.js
@@ -3,22 +3,27 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class RegulatoryStatementsSidebarInsertComponent extends Component {
+  @tracked isDisabled = false;
   @tracked modalEnabled = false;
+  @tracked besluit;
+
+  constructor() {
+    super(...arguments);
+    this.args.controller.onEvent('contentChanged', this.update.bind(this));
+    this.update();
+  }
+
+  update() {
+    this.besluit = this.args.controller.datastore
+      .match(null, 'a', `besluit:Besluit`)
+      .asSubjectNodes()
+      .next().value;
+    this.isDisabled = !this.besluit;
+  }
 
   @action
   toggleModal() {
     this.modalEnabled = !this.modalEnabled;
-  }
-
-  get besluit() {
-    return this.args.controller.datastore
-      .match(null, 'a', `besluit:Besluit`)
-      .asSubjectNodes()
-      .next().value;
-  }
-
-  get isDisabled() {
-    return !this.besluit;
   }
 
   @action
