84e532989d8b500debff9eed0a075be9819b7051
Elena Poelman <elena.poelman@redpencil.io>
2022-11-16 15:34:40 +0100

    replace content by file in versioned-regulatory-statement model

--- app/models/versioned-regulatory-statement.js
+++ app/models/versioned-regulatory-statement.js
@@ -2,10 +2,10 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class VersionedRegulatoryStatementModel extends Model {
   @attr state;
-  @attr content;
   @attr uri;
   @hasMany('signed-resource') signedResources;
   @belongsTo('published-resource') publishedResource;
   @belongsTo('versioned-behandeling') versionedBehandeling;
   @belongsTo('editor-document') regulatoryStatement;
+  @belongsTo('file') file;
 }
