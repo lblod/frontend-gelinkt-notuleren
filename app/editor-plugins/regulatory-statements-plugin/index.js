ba20097986345bb6b90c390db091233063bc938a
Elena Poelman <elena.poelman@redpencil.io>
2022-12-16 10:24:51 +0100

    remove commented code

--- app/editor-plugins/regulatory-statements-plugin/index.js
+++ app/editor-plugins/regulatory-statements-plugin/index.js
@@ -2,34 +2,6 @@ import {
   createEmberNodeSpec,
   createEmberNodeView,
 } from '@lblod/ember-rdfa-editor/utils/ember-node';
-// export default class RegulatoryStatementsViewSpec extends InlineComponentSpec {
-//   matcher = {
-//     tag: this.tag,
-//     attributeBuilder: (node) => {
-//       if (isElement(node)) {
-//         if (node.dataset.inlineComponent === this.name) {
-//           return {};
-//         }
-//       }
-//       return null;
-//     },
-//   };
-//   properties = {
-//     uri: { serializable: true },
-//   };
-//   _renderStatic(props) {
-//     return `<br>
-//             <div resource="${props.uri}" property="eli:related_to" rev="dct:isPartOf" typeof="besluitpublicatie:Documentonderdeel">
-//               <h5>Reglementaire bijlage: ${props.title}</h5>
-//               <div>
-//                 ${props.content}
-//               </div>
-//             </div>`;
-//   }
-//   constructor(controller) {
-//     super('editor-plugins/regulatory-statements/view', 'div', controller);
-//   }
-// }
 
 const emberNodeConfig = {
   name: 'regulatory-statement-view',
