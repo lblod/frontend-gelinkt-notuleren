57e5f8d8b44ef335c9b40aa46c60e48bcedf208d
Elena Poelman <elena.poelman@redpencil.io>
2023-05-15 12:06:56 +0200

    remove tasklist related models

--- app/models/tasklist-solution.js
+++
@@ -1,7 +0,0 @@
-import BaseTasklistSolutionModel from '@lblod/ember-vo-tasklist/models/tasklist-solution';
-import { attr } from '@ember-data/model';
-
-export default class TasklistSolutionModel extends BaseTasklistSolutionModel {
-  @attr uri;
-  @attr('number') index;
-}
--- app/models/tasklist.js
+++
@@ -1,6 +0,0 @@
-import BaseTasklistModel from '@lblod/ember-vo-tasklist/models/tasklist';
-import { attr } from '@ember-data/model';
-
-export default class TasklistModel extends BaseTasklistModel {
-  @attr uri;
-}
