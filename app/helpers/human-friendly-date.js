d267f15acdd217dc37fb7803a084e0070a767763
Elena Poelman <elena.poelman@redpencil.io>
2023-06-01 09:49:45 +0200

    fix humanFriendlyDate helper

--- app/helpers/human-friendly-date.js
+++ app/helpers/human-friendly-date.js
@@ -1,13 +1,14 @@
 import formatRelative from 'date-fns/formatRelative';
 import format from 'date-fns/format';
 import locales from 'date-fns/locale';
+import { helper } from '@ember/component/helper';
 
 function getDateFnsLocale(locale) {
   return locales[locale] ?? locales[locale.substring(0, 2)];
 }
 
-export default function humanFriendlyDate(
-  date,
+function humanFriendlyDate(
+  [date],
   { locale = 'nl-BE', alwaysShowTime = true } = {}
 ) {
   console.log('LOCALE: ', locale);
@@ -29,3 +30,5 @@ export default function humanFriendlyDate(
     return '';
   }
 }
+
+export default helper(humanFriendlyDate);
