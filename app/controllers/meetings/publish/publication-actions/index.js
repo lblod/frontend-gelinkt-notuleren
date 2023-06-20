a4c84d71e4af67950b44c265f1e90c10ecf9b5f7
Elena Poelman <elena.poelman@redpencil.io>
2023-06-20 12:19:39 +0200

    improve publication-actions page translations

--- app/controllers/meetings/publish/publication-actions/index.js
+++ app/controllers/meetings/publish/publication-actions/index.js
@@ -1,8 +1,15 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
-
+import { service } from '@ember/service';
 export default class MeetingsPublishPublicationActionsController extends Controller {
+  @service intl;
   sort = '-date';
   @tracked page = 0;
   @tracked pageSize = 20;
+
+  actionLabel = (log) => {
+    return this.intl.t(
+      `meetings.publish.publication-actions.actions.${log.action}`
+    );
+  };
 }
