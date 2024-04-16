7233151c12dd267248cd7361e8efc1b768c90d78
Rich <piemonkey@protonmail.ch>
2024-04-16 10:42:43 +0200

    Extract fetching file or included content to a util function

--- app/routes/meetings/publish/publication-actions/detail.js
+++ app/routes/meetings/publish/publication-actions/detail.js
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { getResourceContent } from 'frontend-gelinkt-notuleren/utils/get-resource-content';
+
+function onError(statusText) {
+  return `<div class="au-c-alert au-c-alert--warning"><p>Error fetching file contents: ${statusText}</p></div>`;
+}
+
 export default class MeetingsPublishPublicationActionsDetailRoute extends Route {
   @service store;
 
@@ -12,19 +18,11 @@ export default class MeetingsPublishPublicationActionsDetailRoute extends Route 
       },
     );
     const signedResource = await log.signedResource;
-    if (signedResource?.content) {
-      return signedResource.content;
-    } else if (signedResource?.file) {
-      const fileMeta = await signedResource.file;
-      const fileReq = await fetch(fileMeta.downloadLink);
-      if (fileReq.ok) {
-        return fileReq.text();
-      } else {
-        return `<div class="au-c-alert au-c-alert--warning"><p>Error fetching file contents: ${fileReq.statusText}</p></div>`;
-      }
+    if (signedResource) {
+      return getResourceContent(signedResource, onError);
     } else {
       const publishedResource = await log.publishedResource;
-      return publishedResource.content;
+      return getResourceContent(publishedResource, onError);
     }
   }
 }
