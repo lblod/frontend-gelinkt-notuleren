a1f3dc3a0a43edde4b1c123f69d01b0ea1ac4926
abeforgit <arnebertrand@gmail.com>
2024-04-21 21:39:38 +0200

    fix: import snippet insert from correct path
    The problem wasn't the import, the file was simply renamed in the source

--- app/controllers/regulatory-statements/edit.js
+++ app/controllers/regulatory-statements/edit.js
@@ -104,6 +104,7 @@ import {
   OCMW,
 } from '../../utils/bestuurseenheid-classificatie-codes';
 
+import SnippetInsertComponent from '@lblod/ember-rdfa-editor-lblod-plugins/components/snippet-plugin/snippet-insert';
 export default class RegulatoryStatementsRoute extends Controller {
   @service documentService;
   @service store;
@@ -114,6 +115,7 @@ export default class RegulatoryStatementsRoute extends Controller {
   @service intl;
   editor;
   @tracked citationPlugin = citationPlugin(this.config.citation);
+  SnippetInsert = SnippetInsertComponent;
 
   schema = new Schema({
     nodes: {
