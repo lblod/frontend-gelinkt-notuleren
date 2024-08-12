6127729b9cac923835ccfed5750867173072016f
Elena Poelman <elena.poelman@redpencil.io>
2024-08-12 16:48:48 +0200

    add meeting details modal to inauguration meeting creation flow

--- app/routes/inbox/meetings/new.js
+++ app/routes/inbox/meetings/new.js
@@ -6,17 +6,62 @@ export default class InboxMeetingsNewRoute extends Route {
   @service router;
   @service store;
 
+  queryParams = {
+    type: {
+      refreshModel: true,
+    },
+  };
+
   beforeModel() {
     if (!this.currentSession.canWrite) {
       this.router.replaceWith('inbox.meetings');
     }
   }
 
-  model() {
+  model(params) {
+    console.log(params);
+    const type = params.type;
+    switch (type) {
+      case 'common':
+        return this.createCommonMeeting();
+      case 'inauguration-meeting':
+        return this.createInaugurationMeeting();
+    }
+  }
+
+  async createCommonMeeting() {
     let now = new Date();
     return this.store.createRecord('zitting', {
       geplandeStart: now,
       gestartOpTijdstip: now,
+    });
+  }
+
+  async createInaugurationMeeting() {
+    let now = new Date();
+    const bestuursorgaan = (
+      await this.store.query('bestuursorgaan', {
+        include: [
+          'is-tijdsspecialisatie-van.bestuurseenheid',
+          'is-tijdsspecialisatie-van.classificatie',
+        ].join(','),
+        filter: {
+          'is-tijdsspecialisatie-van': {
+            bestuurseenheid: {
+              id: this.currentSession.group.id,
+            },
+            classificatie: {
+              ':uri:':
+                'http://data.vlaanderen.be/id/concept/BestuursorgaanClassificatieCode/5ab0e9b8a3b2ca7c5e000005',
+            },
+          },
+        },
+      })
+    )[0];
+    return this.store.createRecord('installatievergadering', {
+      geplandeStart: now,
+      gestartOpTijdstip: now,
+      bestuursorgaan,
     });
   }
 }
