c657ed6f1597c4b822d2329530143c55b522251a
Rich <piemonkey@protonmail.ch>
2024-10-09 12:30:33 +0200

    Wire up meetings/download/copy breadcrumbs properly

--- app/routes/meetings/download/copy.js
+++ app/routes/meetings/download/copy.js
@@ -4,12 +4,13 @@ import { service } from '@ember/service';
 export default class MeetingsDownloadCopyRoute extends Route {
   @service store;
   async model(params) {
+    const zitting = this.modelFor('meetings.download');
     const container = await this.store.findRecord(
       'document-container',
       params.container_id,
       { include: 'current-version' },
     );
     const document = await container.currentVersion;
-    return { container, document };
+    return { zitting, container, document };
   }
 }
