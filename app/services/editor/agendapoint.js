0bd0edf17e9adecf99c83b8390bf852249e927f9
Elena Poelman <elena.poelman@redpencil.io>
2024-10-29 11:48:31 +0100

    fix: disable rdfa-aware setting on heading nodes

--- app/services/editor/agendapoint.js
+++ app/services/editor/agendapoint.js
@@ -141,7 +141,7 @@ export default class AgendapointEditorService extends Service {
       codelist,
       roadsign_regulation,
       mandatee_table,
-      heading: headingWithConfig({ rdfaAware: true }),
+      heading: headingWithConfig({ rdfaAware: false }),
       blockquote,
       snippet_placeholder: snippetPlaceholder,
       snippet: snippet(this.config.snippet),
