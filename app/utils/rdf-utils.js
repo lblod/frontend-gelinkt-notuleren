8378ab490fa2ba74005d2f0efb8c71045db41f92
Elena Poelman <elena.poelman@redpencil.io>
2024-11-05 14:06:05 +0100

    prettier

--- app/utils/rdf-utils.js
+++ app/utils/rdf-utils.js
@@ -2,7 +2,7 @@
  * Function which returns the last part of a URI (after the '/' or '#')
  * @param {string} uri
  */
-export function getIdentifier(uri){
+export function getIdentifier(uri) {
   const segments = uri.split('/');
   const lastSegment = segments.pop();
   const identifier = lastSegment.split('#').pop();
