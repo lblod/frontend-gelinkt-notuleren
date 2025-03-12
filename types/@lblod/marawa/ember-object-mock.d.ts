78464ce7ae8c42d40b7d314c60840a3ac07d7aaf
abeforgit <arnebertrand@gmail.com>
2025-03-12 09:59:42 +0100

    feat: add marawa types

---
+++ types/@lblod/marawa/ember-object-mock.d.ts
@@ -0,0 +1,1 @@
+export function warn(msg: string): void;
---
+++ types/@lblod/marawa/rdfa-attributes.d.ts
@@ -0,0 +1,23 @@
+export default class RdfaAttributes {
+  constructor(
+    node: { getAttribute(key: string): string | undefined },
+    knownPrefixes?: unknown,
+    options?: Record<string, unknown>,
+  );
+  get vocab(): string;
+  get content(): string;
+  get properties(): string[];
+  get rel(): string[];
+  get typeof(): string[];
+  get rev(): string[];
+  get about(): string;
+  get datatype(): string;
+  get src(): string;
+  get href(): string;
+  get resource(): string;
+  get isEmpty(): boolean;
+  currentPrefixes: Record<string, unknown>;
+}
+export function parsePrefixString(prefixString: string): {
+  [key: string]: string;
+};
