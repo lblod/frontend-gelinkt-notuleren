30cf08897a860878dd8fea1981601d1d7e2bd379
abeforgit <arnebertrand@gmail.com>
2025-05-04 20:58:51 +0200

    feat: add loose insertion of templates relevant for citerra

--- app/controllers/regulatory-statements/edit.js
+++ app/controllers/regulatory-statements/edit.js
@@ -4,6 +4,8 @@ import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency';
 import { service } from '@ember/service';
 import { getOwner } from '@ember/application';
+import { insertHtml } from '@lblod/ember-rdfa-editor/commands/insert-html-command';
+import instantiateUuids from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/standard-template-plugin/utils/instantiate-uuids';
 
 import {
   em,
@@ -407,4 +409,222 @@ export default class RegulatoryStatementsRoute extends Controller {
   LinkRdfaNodeButton = LinkRdfaNodeButton;
   visualizerConfig = RDFA_VISUALIZER_CONFIG;
   backlinkEditorConfig = BACKLINK_EDITOR_CONFIG;
+
+  @action
+  insertThing(thing) {
+    const map = {
+      nummerplaten: /* HTML */ `
+        <div
+          class="say-editable say-block-rdfa"
+          about="http://data.vlaanderen.be/id/voorwaarden/--ref-uuid4-b73cdde4-482a-454a-8323-ce20e04e3ac7"
+          data-say-id="b73cdde4-482a-454a-8323-ce20e04e3ac7"
+          property="http://www.w3.org/ns/prov#value"
+          lang="nl-be"
+          data-pm-slice="0 0 []"
+          ><div
+            style="display: none"
+            class="say-hidden"
+            data-rdfa-container="true"
+            ><span
+              about="http://data.vlaanderen.be/id/voorwaarden/--ref-uuid4-b73cdde4-482a-454a-8323-ce20e04e3ac7"
+              property="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+              resource="http://data.europa.eu/m8g/Requirement"
+            ></span
+            ><span
+              property="http://www.w3.org/2004/02/skos/core#prefLabel"
+              content="Aantal nummerplaten"
+              lang="nl-be"
+            ></span></div
+          ><div data-content-container="true"
+            ><p class="say-paragraph"
+              >Tenzij anders bepaald, kan de vergunning voor een onbeperkt
+              aantal nummerplaten worden aangevraagd</p
+            ></div
+          ></div
+        >
+      `,
+      duurtijd: /* HTML */ `
+        <div
+          class="say-editable say-block-rdfa"
+          about="http://data.vlaanderen.be/id/duurtijden/--ref-uuid4-7586b9c7-e0ed-4a9f-94f6-5705420ec3cf"
+          data-say-id="7586b9c7-e0ed-4a9f-94f6-5705420ec3cf"
+          property="http://www.w3.org/ns/prov#value"
+          lang="nl-be"
+          data-pm-slice="0 0 []"
+          ><div
+            style="display: none"
+            class="say-hidden"
+            data-rdfa-container="true"
+            ><span
+              about="http://data.vlaanderen.be/id/duurtijden/--ref-uuid4-7586b9c7-e0ed-4a9f-94f6-5705420ec3cf"
+              property="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+              resource="http://data.europa.eu/m8g/Requirement"
+            ></span
+            ><span
+              property="http://www.w3.org/2004/02/skos/core#prefLabel"
+              content="Duurtijd"
+              lang="nl-be"
+            ></span></div
+          ><div data-content-container="true"
+            ><p class="say-paragraph"
+              >Tenzij anders bepaald, heeft de vergunning een onbeperkte
+              duurtijd.</p
+            ></div
+          ></div
+        >
+      `,
+      zone: /* HTML */ `
+        <div
+          class="say-editable say-block-rdfa"
+          about="http://data.vlaanderen.be/id/zones/--ref-uuid4-32047e20-00d3-4b66-8f1f-9dc5fa275e0f"
+          data-say-id="32047e20-00d3-4b66-8f1f-9dc5fa275e0f"
+          property="http://www.w3.org/ns/prov#value"
+          lang="nl-be"
+          data-pm-slice="0 0 []"
+          ><div
+            style="display: none"
+            class="say-hidden"
+            data-rdfa-container="true"
+            ><span
+              property="http://www.w3.org/2004/02/skos/core#prefLabel"
+              content="Zone"
+              lang="nl-be"
+            ></span
+            ><span
+              about="http://data.vlaanderen.be/id/zones/--ref-uuid4-32047e20-00d3-4b66-8f1f-9dc5fa275e0f"
+              property="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+              resource="https://data.vlaanderen.be/ns/mobiliteit#Zone"
+            ></span
+            ><span
+              about="http://data.vlaanderen.be/id/zones/--ref-uuid4-32047e20-00d3-4b66-8f1f-9dc5fa275e0f"
+              property="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+              resource="http://data.europa.eu/m8g/Requirement"
+            ></span></div
+          ><div data-content-container="true"><p class="say-paragraph"></p></div
+        ></div>
+      `,
+      bewijsstuk: /* HTML */ `
+        <span
+          class="say-variable"
+          data-say-variable="true"
+          data-say-variable-type="codelist"
+          data-selection-style="single"
+          data-label="bewijsstuk"
+          data-codelist="http://lblod.data.gift/concept-schemes/6810101828455F96985D7CD2"
+          data-source="/raw-sparql"
+          data-say-id="5af9422e-2479-4b3b-8ca6-0a30d66cf254"
+          data-literal-node="true"
+          datatype="http://www.w3.org/2001/XMLSchema#string"
+          ><span
+            style="display: none"
+            class="say-hidden"
+            data-rdfa-container="true"
+          ></span
+          ><span data-content-container="true"
+            ><span
+              class="mark-highlight-manual say-placeholder"
+              placeholdertext="bewijsstuk"
+              contenteditable="false"
+              >bewijsstuk</span
+            ></span
+          ></span
+        >
+      `,
+      voorwaarde: /* HTML */ `
+        <div
+          class="say-editable say-block-rdfa"
+          about="http://data.vlaanderen.be/id/voorwaarden/--ref-uuid4-197c3905-3587-4c33-9765-d34ec7e113a1"
+          data-say-id="d3ecf4b0-f9b5-4ef8-893e-94784f170a61"
+          property="http://www.w3.org/ns/prov#value"
+          lang="nl-be"
+          data-pm-slice="0 0 []"
+          ><div
+            style="display: none"
+            class="say-hidden"
+            data-rdfa-container="true"
+            ><span
+              about="http://data.vlaanderen.be/id/voorwaarden/--ref-uuid4-197c3905-3587-4c33-9765-d34ec7e113a1"
+              property="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+              resource="http://data.europa.eu/m8g/Requirement"
+            ></span
+            ><span
+              property="http://www.w3.org/2004/02/skos/core#prefLabel"
+              content="Voorwaarde"
+              lang="nl-be"
+            ></span
+            ><span
+              rev="http://data.europa.eu/m8g/isRequirementOf"
+              resource="http://collection/1"
+            ></span></div
+          ><div data-content-container="true"><p class="say-paragraph"></p></div
+        ></div>
+      `,
+      doelgroep: /* HTML */ `
+        <div>
+          <div
+            class="say-editable say-block-rdfa"
+            about="http://data.vlaanderen.be/7079c444-a934-4ddf-85d1-f0968b5555dd"
+            data-say-id="7079c444-a934-4ddf-85d1-f0968b5555dd"
+            data-pm-slice="0 0 []"
+            ><div
+              style="display: none"
+              class="say-hidden"
+              data-rdfa-container="true"
+              ><span
+                about="http://data.vlaanderen.be/7079c444-a934-4ddf-85d1-f0968b5555dd"
+                property="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+                resource="http://data.europa.eu/m8g/Requirement"
+              ></span
+              ><span
+                property="http://www.w3.org/2004/02/skos/core#prefLabel"
+                content="Doelgroep"
+                lang="nl-be"
+              ></span></div
+            ><div data-content-container="true"
+              ><p class="say-paragraph"
+                ><span
+                  class="say-variable"
+                  data-say-variable="true"
+                  data-say-variable-type="codelist"
+                  data-selection-style="single"
+                  data-label="type aanvrager"
+                  data-codelist="http://lblod.data.gift/concept-schemes/680FE8AD28455F96985D7CB9"
+                  data-source="/raw-sparql"
+                  data-say-id="0b1fedba-91c9-4d2d-9720-67bc618e8842"
+                  data-literal-node="true"
+                  datatype="http://www.w3.org/2001/XMLSchema#string"
+                  ><span
+                    style="display: none"
+                    class="say-hidden"
+                    data-rdfa-container="true"
+                  ></span
+                  ><span data-content-container="true"
+                    ><span
+                      class="mark-highlight-manual say-placeholder"
+                      placeholdertext="type aanvrager"
+                      contenteditable="false"
+                      >type aanvrager</span
+                    ></span
+                  ></span
+                >
+                waarbij volgende voorwaarden van toepassing zijn:</p
+              ></div
+            ></div
+          ></div
+        >
+      `,
+    };
+    this.controller.doCommand(
+      insertHtml(
+        instantiateUuids(map[thing]),
+        this.controller.mainEditorState.selection.from,
+        this.controller.mainEditorState.selection.to,
+        undefined,
+        false,
+        true,
+      ),
+      { view: this.controller.mainEditorView },
+    );
+    this.controller.withTransaction((tr) => tr.scrollIntoView(false));
+  }
 }
