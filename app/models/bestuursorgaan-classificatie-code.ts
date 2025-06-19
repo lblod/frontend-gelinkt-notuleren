819008aa799aa0dc3093a2e0d1d69390c29ce7d3
Elena Poelman <elena.poelman@redpencil.io>
2025-06-19 11:21:24 +0200

    bestuursorgaan-classificatie-code: add type member

--- app/models/bestuursorgaan-classificatie-code.ts
+++ app/models/bestuursorgaan-classificatie-code.ts
@@ -2,8 +2,10 @@ import type { AsyncHasMany } from '@ember-data/model';
 import Model, { attr, hasMany } from '@ember-data/model';
 import type BestuursfunctieCodeModel from './bestuursfunctie-code';
 import type BestuursorgaanModel from './bestuursorgaan';
+import type { Type } from '@warp-drive/core-types/symbols';
 
 export default class BestuursorgaanClassificatieCodeModel extends Model {
+  declare [Type]: 'bestuursorgaan-classificatie-code';
   @attr uri?: string;
   @attr label?: string;
   @attr scopeNote?: string;
