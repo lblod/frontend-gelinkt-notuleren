2229aedeac4dfa00d53d3bcc154588ffcbf07c3c
abeforgit <arnebertrand@gmail.com>
2026-07-01 19:51:02 +0200

    fix: make sure the edit route loads the new version

--- app/routes/meetings/edit/agendapoint/edit.ts
+++ app/routes/meetings/edit/agendapoint/edit.ts
@@ -9,13 +9,22 @@ export default class MeetingsEditAgendapointEditRoute extends Route {
   @service declare standardTemplate: StandardTemplateService;
 
   async model() {
-    const parentModel = this.modelFor(
+    console.log('agendapoint edit route hook running');
+
+    // deliberately not using the editorDocument from the parent route, since
+    // this.modelFor does not rerun the parent's route hook if it's already loaded.
+    // So if we transition here from a sibling route, such as the .new route,
+    // we might have an out-of-date currentVersion on the container
+    const { returnToMeeting, documentContainer } = this.modelFor(
       'meetings.edit.agendapoint',
     ) as ModelFrom<MeetingsEditAgendapointRoute>;
     const templates = this.standardTemplate.fetchTemplates.perform();
 
     return RSVP.hash({
-      ...parentModel,
+      returnToMeeting,
+      documentContainer,
+      // asking ember-data to fetch the currentVersion again
+      editorDocument: documentContainer.currentVersion,
       // This does not include dynamic templates as it is only used for the standard template plugin
       templates,
     });
